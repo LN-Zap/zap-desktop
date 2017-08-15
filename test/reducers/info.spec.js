170dc13b5b5f9e09542bac6407aab9d5d0a053d2
Jack Mallers <jimmymowschess@gmail.com>
2017-08-15 17:56:01 -0500

    feature(info): add basic tests for info reducer

---
+++ test/reducers/info.spec.js
@@ -0,0 +1,28 @@
+import infoReducer, {
+  GET_INFO,
+  RECEIVE_INFO
+} from '../../app/reducers/info'
+
+describe('reducers', () => {
+  describe('infoReducer', () => {
+    it('should handle initial state', () => {
+      expect(infoReducer(undefined, {})).toMatchSnapshot()
+    })
+
+    it('should have GET_INFO', () => {
+      expect(GET_INFO).toEqual('GET_INFO')
+    })
+
+    it('should have RECEIVE_INFO', () => {
+      expect(RECEIVE_INFO).toEqual('RECEIVE_INFO')
+    })
+
+    it('should correctly getInfo', () => {
+      expect(infoReducer(undefined, { type: GET_INFO })).toMatchSnapshot()
+    })
+
+    it('should correctly receiveInfo', () => {
+      expect(infoReducer(undefined, { type: RECEIVE_INFO, data: 'foo' })).toMatchSnapshot()
+    })
+  })
+})
