28fff8101dcaf5486796746c53ace5a153e53d1b
Jack Mallers <jimmymowschess@gmail.com>
2017-08-15 17:44:50 -0500

    feature(channels): add basic test for channels reducer

--- test/reducers/channels.spec.js
+++ test/reducers/channels.spec.js
@@ -13,5 +13,53 @@ describe('reducers', () => {
     it('should handle initial state', () => {
       expect(channelsReducer(undefined, {})).toMatchSnapshot()
     })
+
+    it('should have SET_CHANNEL_FORM', () => {
+      expect(SET_CHANNEL_FORM).toEqual('SET_CHANNEL_FORM')
+    })
+
+    it('should have SET_CHANNEL', () => {
+      expect(SET_CHANNEL).toEqual('SET_CHANNEL')
+    })
+
+    it('should have GET_CHANNELS', () => {
+      expect(GET_CHANNELS).toEqual('GET_CHANNELS')
+    })
+
+    it('should have RECEIVE_CHANNELS', () => {
+      expect(RECEIVE_CHANNELS).toEqual('RECEIVE_CHANNELS')
+    })
+
+    it('should have OPENING_CHANNEL', () => {
+      expect(OPENING_CHANNEL).toEqual('OPENING_CHANNEL')
+    })
+
+    it('should have OPENING_SUCCESSFUL', () => {
+      expect(OPENING_SUCCESSFUL).toEqual('OPENING_SUCCESSFUL')
+    })
+
+    it('should have OPENING_FAILURE', () => {
+      expect(OPENING_FAILURE).toEqual('OPENING_FAILURE')
+    })
+
+    it('should correctly setChannel', () => {
+      expect(channelsReducer(undefined, { type: SET_CHANNEL, channel: 'channel' })).toMatchSnapshot()
+    })
+
+    it('should correctly setChannelForm', () => {
+      expect(channelsReducer(undefined, { type: SET_CHANNEL_FORM, form: { isOpen: true } })).toMatchSnapshot()
+    })
+
+    it('should correctly setChannelForm', () => {
+      expect(channelsReducer(undefined, { type: GET_CHANNELS })).toMatchSnapshot()
+    })
+
+    it('should correctly setChannelForm', () => {
+      expect(channelsReducer(undefined, { type: RECEIVE_CHANNELS, channels: [1, 2], pendingChannels: [3, 4] })).toMatchSnapshot()
+    })
+
+    it('should correctly setChannelForm', () => {
+      expect(channelsReducer(undefined, { type: OPENING_CHANNEL })).toMatchSnapshot()
+    })
   })
 })
