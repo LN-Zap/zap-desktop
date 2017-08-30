6b5e12fe8142b9d08cd94d58dff388f76c71d1f4
Nigel Christian <callmekurisu@gmail.com>
2017-08-29 23:23:14 -1000

    Changed linux instruction for tls.cert
    ~/.lnd/tls.cert path did not work for tls.cert but /home/user/.lnd/tls.cert did

--- app/lnd/config/index.js
+++ app/lnd/config/index.js
@@ -1,6 +1,6 @@
 // Cert will be located depending on your machine
 // Mac OS X: /Users/user/Library/Application Support/Lnd/tls.cert
-// Linux: ~/.lnd/tls.cert
+// Linux: /home/user/.lnd/tls.cert
 // Windows: TODO find out where cert is located for windows machine
 export default {
   lightningRpc: `${__dirname}/rpc.proto`,
