5dcf9d97fc74e25ac718277fe5649b68a222dd46
Jason Tarre <jason.tarre@gmail.com>
2017-09-07 12:30:58 -0400

    fix(spellng correction): change 'Staring Main Process...' to 'Starting Main Process...'.

--- webpack.config.renderer.dev.js
+++ webpack.config.renderer.dev.js
@@ -267,7 +267,7 @@ export default merge.smart(baseConfig, {
     },
     setup() {
       if (process.env.START_HOT) {
-        console.log('Staring Main Process...');
+        console.log('Starting Main Process...');
         spawn(
           'npm',
           ['run', 'start-main-dev'],
