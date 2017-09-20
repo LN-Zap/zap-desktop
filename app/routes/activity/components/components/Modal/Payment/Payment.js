beb23448db3e9c35c51f7afc0e6d76d8925d0ca3
Jack Mallers <jimmymowschess@gmail.com>
2017-09-20 18:47:50 -0500

    feature(payment, transaction): style payment and transaction modals

---
+++ app/routes/activity/components/components/Modal/Payment/Payment.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Moment from 'react-moment'
+import 'moment-timezone'
+
+import QRCode from 'qrcode.react'
+
+import { MdCheck } from 'react-icons/lib/md'
+
+import CurrencyIcon from '../../../../../../components/CurrencyIcon'
+import { btc } from '../../../../../../utils'
+
+import styles from './Payment.scss'
+
+
+const Payment = ({ payment, ticker, currentTicker }) => (
+  <div className={styles.container}>
+    <h3>{payment.payment_hash}</h3>
+    <h1>
+      <CurrencyIcon currency={ticker.currency} crypto={ticker.crypto} styles={{ verticalAlign: 'top' }} />
+      <span className={styles.value}>
+        {
+          ticker.currency === 'usd' ?
+            btc.satoshisToUsd(payment.value, currentTicker.price_usd)
+            :
+            btc.satoshisToBtc(payment.value)
+        }
+      </span>
+    </h1>
+    <dl>
+      <dt>Fee</dt>
+      <dd>{payment.fee}</dd>
+      <dt>Date</dt>
+      <dd>
+        <Moment format='MMM Do'>{payment.creation_date * 1000}</Moment>
+      </dd>
+    </dl>
+  </div>
+)
+
+Payment.propTypes = {
+}
+
+export default Payment
---
+++ app/routes/activity/components/components/Modal/Transaction/Transaction.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Moment from 'react-moment'
+import 'moment-timezone'
+
+import QRCode from 'qrcode.react'
+
+import { MdCheck } from 'react-icons/lib/md'
+
+import CurrencyIcon from '../../../../../../components/CurrencyIcon'
+import { btc } from '../../../../../../utils'
+
+import styles from './Transaction.scss'
+
+
+const Transaction = ({ transaction, ticker, currentTicker }) => (
+  <div className={styles.container}>
+    <h2>
+      {
+        transaction.amount < 0 ?
+          'Sent'
+          :
+          'Received'
+      }
+    </h2>
+    <h3>{transaction.tx_hash}</h3>
+    <h1>
+      <CurrencyIcon currency={ticker.currency} crypto={ticker.crypto} styles={{ verticalAlign: 'top' }} />
+      <span className={styles.value}>
+        {
+          ticker.currency === 'usd' ?
+            btc.satoshisToUsd(transaction.amount, currentTicker.price_usd)
+            :
+            btc.satoshisToBtc(transaction.amount)
+        }
+      </span>
+    </h1>
+    <dl>
+      <dt>Confirmations</dt>
+      <dd>{transaction.num_confirmations}</dd>
+      <dt>Fee</dt>
+      <dd>{transaction.total_fees}</dd>
+      <dt>Date</dt>
+      <dd>
+        <Moment format='MMM Do'>{transaction.time_stamp * 1000}</Moment>
+      </dd>
+    </dl>
+  </div>
+)
+
+Transaction.propTypes = {
+}
+
+export default Transaction
