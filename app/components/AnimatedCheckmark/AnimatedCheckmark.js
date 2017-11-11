f2f211c0d7171b34e757fb667fc98e64ace4f624
Jack Mallers <jimmymowschess@gmail.com>
2017-11-11 00:39:49 -0600

    fix(animated checkmark path was wrong lol)

--- app/components/AnimatedCheckmark/AnimatedCheckmark.js
+++ app/components/AnimatedCheckmark/AnimatedCheckmark.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Isvg from 'react-inlinesvg'
-import checkmarkIcon from './components/AnimatedCheckmark/checkmark.svg'
+import checkmarkIcon from 'components/AnimatedCheckmark/checkmark.svg'
 
 const AnimatedCheckmark = () => <Isvg src={checkmarkIcon} />
 
