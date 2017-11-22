0e5141087997f56b97d37b222c03e2704241e936
Torkel Rogstad <torkelrog@gmail.com>
2017-11-22 22:37:43 +0100

    Checks for Linux/Mac cert location

--- app/lnd/config/index.js
+++ app/lnd/config/index.js
@@ -2,10 +2,17 @@
 // Mac OS X: /Users/user/Library/Application Support/Lnd/tls.cert
 // Linux: ~/.lnd/tls.cert
 // Windows: TODO find out where cert is located for windows machine
-import { userInfo } from 'os'
+import { userInfo, platform } from 'os'
+
+var loc;
+if (platform() == 'linux') {
+  loc = `/home/${userInfo().username}/.lnd/tls.cert`
+} else if (platform() == 'darwin') {
+  loc = `/Users/${userInfo().username}/Library/Application Support/lnd/tls.cert`
+}
 
 export default {
   lightningRpc: `${__dirname}/rpc.proto`,
   lightningHost: 'localhost:10009',
-  cert: `/Users/${userInfo().username}/Library/Application Support/Lnd/tls.cert`
+  cert: loc
 }
