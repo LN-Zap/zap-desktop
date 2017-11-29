af95c57bf3072f3896a1fbe4a3b3fbdaf811c131
Jack Mallers <jimmymowschess@gmail.com>
2017-11-29 12:25:18 -0600

    feature(peer form): wire up peer form

--- app/routes/peers/components/Peers.js
+++ app/routes/peers/components/Peers.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+
+import { MdSearch } from 'react-icons/lib/md'
+
+import PeerForm from 'components/Peers/PeerForm'
+
 import styles from './Peers.scss'
 
 class Peers extends Component {
   render() {
+    const { 
+      peerFormProps,
+      setPeerForm,
+      peers: { peers }
+    } = this.props
+    console.log('props: ', this.props)
+
     return (
-      <h1>peers</h1>
+      <div>
+        <div className={styles.search}>
+          <PeerForm {...peerFormProps} />
+
+          <label className={`${styles.label} ${styles.input}`} htmlFor='channelSearch'>
+            <MdSearch />
+          </label>
+          <input
+            value={''}
+            onChange={event => console.log('event: ', event)}
+            className={`${styles.text} ${styles.input}`}
+            placeholder='Search peers by their node public key'
+            type='text'
+            id='peersSearch'
+          />
+        </div>
+
+        <header className={styles.header}>
+          <div className={styles.addPeerContainer}>
+            <div className={`buttonPrimary ${styles.newPeerButton}`} onClick={() => setPeerForm({ isOpen: true })}>
+              Add new peer
+            </div>
+          </div>
+        </header>
+
+        <div className={styles.peers}>
+
+        </div>
+      </div>
     )
   }
 }
--- app/routes/peers/containers/PeersContainer.js
+++ app/routes/peers/containers/PeersContainer.js
@@ -1,14 +1,45 @@
 import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
 
+import { 
+  fetchPeers,
+  setPeer,
+  peersSelectors,
+  setPeerForm,
+  connectRequest,
+  disconnectRequest
+} from 'reducers/peers'
+
 import Peers from '../components/Peers'
 
 const mapDispatchToProps = {
-
+  fetchPeers,
+  setPeer,
+  peersSelectors,
+  setPeerForm,
+  connectRequest,
+  disconnectRequest
 }
 
 const mapStateToProps = state => ({
   peers: state.peers
 })
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Peers))
+const mergeProps = (stateProps, dispatchProps, ownProps) => {
+  const peerFormProps = {
+    setForm: dispatchProps.setPeerForm,
+    connect: dispatchProps.connectRequest,
+
+    form: stateProps.peers.peerForm
+  }
+
+  return {
+    ...stateProps,
+    ...dispatchProps,
+    ...ownProps,
+
+    peerFormProps
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps, mergeProps)(Peers))
