e51a2042dfaca9775ec2e6bde162e11f7ef37d17
Jack Mallers <jimmymowschess@gmail.com>
2017-12-29 10:20:27 -0600

    hotfix(global error): only setTimeout when theres an error

--- app/components/GlobalError/GlobalError.js
+++ app/components/GlobalError/GlobalError.js
@@ -3,21 +3,28 @@ import PropTypes from 'prop-types'
 import { MdClose } from 'react-icons/lib/md'
 import styles from './GlobalError.scss'
 
-const GlobalError = ({ error, clearError }) => {
-  setTimeout(clearError, 10000)
+class GlobalError extends React.Component {
+  componentDidUpdate(prevProps) {
+    if (!prevProps.error && this.props.error) {
+      setTimeout(this.props.clearError, 10000)
+    }
+  }
 
-  return (
-    <div className={`${styles.container} ${!error && styles.closed}`}>
-      <div className={styles.content}>
-        <div className={styles.close} onClick={clearError}>
-          <MdClose />
+  render() {
+    const { error, clearError } = this.props
+
+    return (
+      <div className={`${styles.container} ${!error && styles.closed}`}>
+        <div className={styles.content}>
+          <div className={styles.close} onClick={clearError}>
+            <MdClose />
+          </div>
+          <h2>{error}</h2>
         </div>
-        <h2>{error}</h2>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
 
 GlobalError.propTypes = {
   error: PropTypes.string,
