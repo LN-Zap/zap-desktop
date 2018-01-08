24216733795874418ba5c2b29191129db45a1fb5
Ben Woosley <ben.woosley@gmail.com>
2018-01-08 14:39:31 -0800

    enhance(form-tests): Test that Pay Form display respects isOnchain / isLn props

--- test/components/Form/Payform.spec.js
+++ test/components/Form/Payform.spec.js
@@ -38,4 +38,26 @@ describe('Form', () => {
       expect(el.contains('on-chain')).toBe(false)
     })
   })
+
+  describe('should show lightning with a lightning input', () => {
+    const props = { ...defaultProps, isLn: true }
+    const el = shallow(<PayForm {...props} />)
+
+    it('should contain PayForm', () => {
+      expect(el.find('input#paymentRequest').props.value).toBe(undefined)
+      expect(el.contains('lightning network')).toBe(true)
+      expect(el.contains('on-chain')).toBe(false)
+    })
+  })
+
+  describe('should show on-chain with an on-chain input', () => {
+    const props = { ...defaultProps, isOnchain: true }
+    const el = shallow(<PayForm {...props} />)
+
+    it('should contain PayForm', () => {
+      expect(el.find('input#paymentRequest').props.value).toBe(undefined)
+      expect(el.contains('lightning network')).toBe(false)
+      expect(el.contains('on-chain')).toBe(true)
+    })
+  })
 })
