3397808513492b66b2ba407a8c8dd436676b3dff
Fredrik Johansson <fredrik.johansson@thanx.se>
2018-01-12 09:35:12 +0100

    fix(dynamic-currency-field): update styling and add ghost input for correct width to remove cutting

--- app/components/Form/PayForm.js
+++ app/components/Form/PayForm.js
@@ -8,16 +8,31 @@ import CurrencyIcon from 'components/CurrencyIcon'
 import styles from './PayForm.scss'
 
 class PayForm extends Component {
-  componentDidUpdate(prevProps) {
-    const {
-      isOnchain, isLn, payform: { payInput }, fetchInvoice
-    } = this.props
+  constructor(props) {
+    super(props)
+    this.state = { scrollWidth: 0 }
+  }
+
+  componentDidMount() {
+    // After amountGhostInput is rendered, set scrollWidth
+    this.setState({ scrollWidth: this.amountGhostInput.scrollWidth })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { isOnchain, isLn, payform: { payInput }, fetchInvoice } = this.props
 
     // If on-chain, focus on amount to let user know it's editable
-    if (isOnchain) { this.amountInput.focus() }
+    if (isOnchain) {
+      this.amountInput.focus()
+    }
+
+    // If the with of the ghost input has changed, update the scrollWith
+    if (prevState.scrollWidth != this.amountGhostInput.scrollWidth) {
+      this.setState({ scrollWidth: this.amountGhostInput.scrollWidth })
+    }
 
     // If LN go retrieve invoice details
-    if ((prevProps.payform.payInput !== payInput) && isLn) {
+    if (prevProps.payform.payInput !== payInput && isLn) {
       fetchInvoice(payInput)
     }
   }
@@ -44,54 +59,59 @@ class PayForm extends Component {
       onPaySubmit
     } = this.props
 
+    const { scrollWidth } = this.state
+
+    const fontSize = `${190 - amount.length ** 2}px`
+
     return (
       <div className={styles.container}>
         {showPayLoadingScreen && <LoadingBolt />}
 
         <section className={`${styles.amountContainer} ${isLn ? styles.ln : ''} ${showErrors.amount && styles.error}`}>
+          <span className={styles.ghostInput} ref={input => (this.amountGhostInput = input)} style={{ fontSize }}>
+            {this.amountInput && this.amountInput.value != '' ? this.amountInput.value : 0}
+          </span>
           <label htmlFor='amount'>
             <CurrencyIcon currency={currency} crypto={crypto} />
           </label>
           <input
             type='number'
             min='0'
-            ref={(input) => { this.amountInput = input }}
+            ref={input => (this.amountInput = input)} // eslint-disable-line
             size=''
-            style={
-              isLn ?
-                { width: '75%', fontSize: '85px' }
-                :
-                { width: `${amount.length > 1 ? (amount.length * 20) - 5 : 35}%`, fontSize: `${190 - (amount.length ** 2)}px` }
-            }
+            style={isLn ? { width: '75%', fontSize: '85px' } : { width: `${scrollWidth + 40}px`, fontSize }}
             value={currentAmount}
             onChange={event => setPayAmount(event.target.value)}
-            onBlur={onPayAmountBlur}
+            onBlur={(event) => {
+              if (event.target.value === '') {
+                setPayAmount('0')
+              }
+              onPayAmountBlur()
+            }}
             id='amount'
             readOnly={isLn}
           />
         </section>
         <section className={`${styles.errorMessage} ${showErrors.amount && styles.active}`}>
-          {showErrors.amount &&
-            <span>{errors.amount}</span>
-          }
+          {showErrors.amount && <span>{errors.amount}</span>}
         </section>
         <div className={styles.inputContainer}>
           <div className={styles.info}>
             <span>{inputCaption}</span>
           </div>
           <aside className={styles.paymentIcon}>
-            {isOnchain &&
+            {isOnchain && (
               <i>
                 <span>on-chain</span>
                 <FaChain />
               </i>
-            }
-            {isLn &&
+            )}
+            {isLn && (
               <i>
                 <span>lightning network</span>
                 <FaBolt />
               </i>
-            }
+            )}
           </aside>
           <section className={`${styles.input} ${showErrors.payInput && styles.error}`}>
             <input
@@ -104,19 +124,18 @@ class PayForm extends Component {
             />
           </section>
           <section className={`${styles.errorMessage} ${showErrors.payInput && styles.active}`}>
-            {showErrors.payInput &&
-              <span>{errors.payInput}</span>
-            }
+            {showErrors.payInput && <span>{errors.payInput}</span>}
           </section>
         </div>
         <section className={styles.buttonGroup}>
-          <div className={`buttonPrimary ${styles.button} ${isValid && styles.active}`} onClick={onPaySubmit}>Pay</div>
+          <div className={`buttonPrimary ${styles.button} ${isValid && styles.active}`} onClick={onPaySubmit}>
+            Pay
+          </div>
         </section>
       </div>
     )
   }
 }
-
 
 PayForm.propTypes = {
   payform: PropTypes.shape({
@@ -129,10 +148,7 @@ PayForm.propTypes = {
 
   isOnchain: PropTypes.bool.isRequired,
   isLn: PropTypes.bool.isRequired,
-  currentAmount: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number
-  ]).isRequired,
+  currentAmount: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   inputCaption: PropTypes.string.isRequired,
   showPayLoadingScreen: PropTypes.bool.isRequired,
   payFormIsValid: PropTypes.shape({
