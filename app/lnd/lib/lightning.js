8e06ad51051d7f84365593c453792b45c40119ae
Ben Woosley <ben.woosley@gmail.com>
2018-01-12 15:51:57 -0800

    fix(eslint-import): Fix that eslint-plugin-import was not active
    For the node import resolver, "paths" is meant to be absolute paths
    for global modules.
    https://github.com/benmosher/eslint-plugin-import/tree/master/resolvers/node

--- app/lnd/lib/lightning.js
+++ app/lnd/lib/lightning.js
@@ -3,10 +3,12 @@ import path from 'path'
 import grpc from 'grpc'
 import config from '../config'
 
-module.exports = (rpcpath, host) => {
+const lightning = (rpcpath, host) => {
   const lndCert = fs.readFileSync(config.cert)
   const credentials = grpc.credentials.createSsl(lndCert)
   const rpc = grpc.load(path.join(__dirname, 'rpc.proto'))
 
   return new rpc.lnrpc.Lightning(host, credentials)
 }
+
+export default { lightning }
--- app/utils/index.js
+++ app/utils/index.js
@@ -1,11 +1,4 @@
-import btc from './btc'
-import usd from './usd'
-import bech32 from './bech32'
-import blockExplorer from './blockExplorer'
-
-export default {
-  btc,
-  usd,
-  bech32,
-  blockExplorer
-}
+export btc from './btc'
+export usd from './usd'
+export bech32 from './bech32'
+export blockExplorer from './blockExplorer'
