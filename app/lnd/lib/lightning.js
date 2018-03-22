c1f9ec58bc8b82082121a3cf27108a9f62bddd44
Jack Mallers <jimmymowschess@gmail.com>
2018-03-22 12:20:02 -0500

    fix(grpcInit): remove old file

--- app/lnd/lib/lightning.js
+++ app/lnd/lib/lightning.js
@@ -19,8 +19,6 @@ process.env.GRPC_SSL_CIPHER_SUITES = process.env.GRPC_SSL_CIPHER_SUITES || [
 ].join(':')
 
 const lightning = (rpcpath, host) => {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-  process.env.GRPC_SSL_CIPHER_SUITES = 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384'
   const lndCert = fs.readFileSync(config.cert)
   const credentials = grpc.credentials.createSsl(lndCert)
   const rpc = grpc.load(path.join(__dirname, 'rpc.proto'))
