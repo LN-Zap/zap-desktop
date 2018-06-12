101dc21534cff938bea0f623caa47b68b32ed0d3
Jack Mallers <jimmymowschess@gmail.com>
2018-06-11 19:59:41 -0500

    fix(connection-type): improve connection type copy

--- app/components/Onboarding/Login.js
+++ app/components/Onboarding/Login.js
@@ -29,7 +29,7 @@ const Login = ({
             unlockingWallet ?
               <i className={styles.spinner} />
               :
-              'Log In'
+              'Unlock'
           }
         </span>
       </div>
