6b13d9a445d8852fd141d12b636048ccdbbf88e6
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2018-07-22 17:22:00 +0200

    fix(onboarding): improve custom connection help
    Don't use placeholder for form field help text as this is only visible
    when the form field is empty. Instead, add help text below the fields
    so that it is always visible to the user in order to help them ensure
    they are entering the correct values.

--- app/components/Onboarding/ConnectionDetails.js
+++ app/components/Onboarding/ConnectionDetails.js
@@ -19,12 +19,14 @@ const ConnectionDetails = ({
       <input
         type="text"
         id="connectionHost"
-        placeholder="Hostname / Port of the Lnd gRPC interface"
         className={`${styles.host} ${startLndHostError && styles.error}`}
         ref={input => input}
         value={connectionHost}
         onChange={event => setConnectionHost(event.target.value)}
       />
+      <p className={styles.description}>
+        Hostname and port of the Lnd gRPC interface. Example: localhost:10009
+      </p>
       <p className={`${startLndHostError && styles.visible} ${styles.errorMessage}`}>
         {startLndHostError}
       </p>
@@ -34,12 +36,12 @@ const ConnectionDetails = ({
       <input
         type="text"
         id="connectionCert"
-        placeholder="Path to the lnd tls cert"
         className={`${styles.cert} ${startLndCertError && styles.error}`}
         ref={input => input}
         value={connectionCert}
         onChange={event => setConnectionCert(event.target.value)}
       />
+      <p className={styles.description}>Path to the lnd tls cert. Example: /path/to/tls.cert</p>
       <p className={`${startLndCertError && styles.visible} ${styles.errorMessage}`}>
         {startLndCertError}
       </p>
@@ -49,12 +51,14 @@ const ConnectionDetails = ({
       <input
         type="text"
         id="connectionMacaroon"
-        placeholder="Path to the lnd macaroon file"
         className={`${styles.macaroon} ${startLndMacaroonError && styles.error}`}
         ref={input => input}
         value={connectionMacaroon}
         onChange={event => setConnectionMacaroon(event.target.value)}
       />
+      <p className={styles.description}>
+        Path to the lnd macaroon file. Example: /path/to/admin.macaroon
+      </p>
       <p className={`${startLndMacaroonError && styles.visible} ${styles.errorMessage}`}>
         {startLndMacaroonError}
       </p>
