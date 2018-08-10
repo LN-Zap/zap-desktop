2870555ab81b71d47ed3314bf26682c8fbf8b5b2
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2018-08-10 15:06:43 +0200

    feat(lnd): update to new btcd backend

--- app/lib/lnd/neutrino.js
+++ app/lib/lnd/neutrino.js
@@ -55,6 +55,14 @@ class Neutrino extends EventEmitter {
       `${this.lndConfig.alias ? `--alias=${this.lndConfig.alias}` : ''}`
     ]
 
+    if (this.lndConfig.network === 'mainnet') {
+      neutrinoArgs.push('--neutrino.connect=mainnet1-btcd.zaphq.io')
+      neutrinoArgs.push('--neutrino.connect=mainnet2-btcd.zaphq.io')
+    } else {
+      neutrinoArgs.push('--neutrino.connect=testnet1-btcd.zaphq.io')
+      neutrinoArgs.push('--neutrino.connect=testnet2-btcd.zaphq.io')
+    }
+
     this.process = spawn(this.lndConfig.binaryPath, neutrinoArgs)
       .on('error', error => this.emit(ERROR, error))
       .on('close', code => {
