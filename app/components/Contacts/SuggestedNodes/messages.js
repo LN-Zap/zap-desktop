56229525ef136fb4805a779478fcce0afd3257fb
Tom Kirkpatrick <tom@systemseed.com>
2018-10-01 21:47:08 +0200

    fix(channels): handle suggested nodes load failure
    If we are unable to load the suggested nodes list, display a message
    prompting the user to open channels.
    
    Previously we would show an infinite spinner.
    
    Fix #392

--- app/components/Contacts/SuggestedNodes/messages.js
+++ app/components/Contacts/SuggestedNodes/messages.js
@@ -4,5 +4,7 @@ import { defineMessages } from 'react-intl'
 export default defineMessages({
   connect: 'Connect',
   empty_description:
-    "Hmmm, looks like you don't have any channels yet. Here are some suggested nodes to open a channel with to get started"
+    "Hmmm, looks like you don't have any channels yet. Here are some suggested nodes to open a channel with to get started",
+  empty_description_alt:
+    "Hmmm, looks like you don't have any channels yet. Open some channels to get started."
 })
