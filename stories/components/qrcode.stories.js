54c790d8a682560d7e4c4bcf8c4da9e4e5738b32
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2018-10-07 21:33:48 +0200

    feat(ui): add QRCode component

---
+++ stories/components/qrcode.stories.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { QRCode } from 'components/UI'
+
+storiesOf('Components.QR Code', module).add('QR Code', () => (
+  <QRCode value="w35x2gzfdech26tnd96xjan9yqks6cqzysxqr23s8wgz42uvfa833nhl75vxewpmsu3up7dnw5pl3nurp0tcagsl6lg5f3ty" />
+))
