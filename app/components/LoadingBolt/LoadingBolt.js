df6e2cb070bfb3a6ee736a3b99ce688dbc6e8481
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2018-10-30 08:50:09 +0100

    chore(deps): update react and react components

--- app/components/LoadingBolt/LoadingBolt.js
+++ app/components/LoadingBolt/LoadingBolt.js
@@ -1,38 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Transition, animated } from 'react-spring'
+import { animated, Transition } from 'react-spring'
 import CloudLightning from 'components/Icon/CloudLightning'
-
 import { FormattedMessage } from 'react-intl'
 import messages from './messages'
-
 import styles from './LoadingBolt.scss'
 
-class LoadingBolt extends React.Component {
+class LoadingBolt extends React.PureComponent {
   static propTypes = {
     isLoading: PropTypes.bool.isRequired,
     theme: PropTypes.object.isRequired
   }
 
   render() {
-    const { theme, isLoading = true } = this.props
+    const { theme, isLoading } = this.props
 
     return (
-      <div>
-        <Transition enter={{ opacity: 1 }} leave={{ opacity: 0 }} native>
-          {isLoading &&
-            (springStyles => (
-              <animated.div style={springStyles} className={`${styles.container} ${theme.name}`}>
-                <div className={styles.content}>
-                  <CloudLightning height="155px" width="150px" />
-                  <h1>
-                    <FormattedMessage {...messages.loading} />
-                  </h1>
-                </div>
-              </animated.div>
-            ))}
-        </Transition>
-      </div>
+      <Transition
+        native
+        items={isLoading}
+        from={{ opacity: 1 }}
+        enter={{ opacity: 1 }}
+        leave={{ opacity: 0 }}
+      >
+        {show =>
+          show &&
+          (springStyles => (
+            <animated.div style={springStyles} className={`${styles.container} ${theme.name}`}>
+              <div className={styles.content}>
+                <CloudLightning height="155px" width="150px" />
+                <h1>
+                  <FormattedMessage {...messages.loading} />
+                </h1>
+              </div>
+            </animated.div>
+          ))
+        }
+      </Transition>
     )
   }
 }
