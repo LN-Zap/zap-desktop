21bcf2229f39a5b69fc9a3b2f91871e9ac144b8a
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2018-11-17 16:49:09 +0100

    style(icons): update icons
    Update icons form latest designs.

--- app/components/Icon/PlusCircle.js
+++ app/components/Icon/PlusCircle.js
@@ -1,20 +1,11 @@
 import React from 'react'
 
 const SvgPlusCircle = props => (
-  <svg
-    width="1em"
-    height="1em"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth={2}
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="plus-circle_svg__feather plus-circle_svg__feather-plus-circle"
-    {...props}
-  >
-    <circle cx={12} cy={12} r={10} />
-    <path d="M12 8v8M8 12h8" />
+  <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+    <g fill="none" fillRule="evenodd" stroke="currentColor" transform="translate(1 1)">
+      <path d="M11.121 7v8M7 11.121h8" strokeLinecap="round" strokeLinejoin="round" />
+      <circle cx={11} cy={11} r={11} />
+    </g>
   </svg>
 )
 
