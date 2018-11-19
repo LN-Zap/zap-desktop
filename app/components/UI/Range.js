d8ea0b46febdd3f66df682d570d417f45cbc4f40
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2018-11-19 23:29:23 +0100

    style: update Range component styles

--- app/components/UI/Range.js
+++ app/components/UI/Range.js
@@ -2,55 +2,103 @@ import React from 'react'
 import { asField } from 'informed'
 import styled, { withTheme } from 'styled-components'
 
-const Input = styled.input`
-  overflow: hidden;
-  width: 100%;
-  appearance: none;
-  outline: none;
+function makelongshadow(color, size, width) {
+  let val = `4px 0px 0 ${size} ${color}`
+  for (let i = 5; i < width; i++) {
+    val += `, ${i}px 0px 0 ${size} ${color}`
+  }
+  val += ', 0 2px 4px 0 rgba(0, 0, 0, 0.5)'
+  return val
+}
 
-  ::-webkit-slider-runnable-track {
-    height: 8px;
-    appearance: none;
-    background-color: ${props => props.theme.colors.gray};
-    margin-top: -1px;
+const Input = styled.input`
+  appearance: none;
+  background: none;
+  cursor: pointer;
+  display: inline-block;
+  height: 100%;
+  min-height: 20px;
+  overflow: hidden;
+  width: ${props => props.sliderWidth};
+
+  &:focus {
+    box-shadow: none;
+    outline: none;
   }
 
-  ::-webkit-slider-thumb {
-    width: 8px;
+  &::-webkit-slider-runnable-track {
+    background: ${props => props.theme.colors[props.backgroundFilledSlider]};
+    content: '';
+    height: ${props => props.sliderHeight};
+    pointer-events: none;
+    border: ${props => props.sliderBorderWidth} solid ${props => props.sliderBorderColor};
+    border-radius: ${props => props.sliderBorderRadius};
+  }
+
+  &::-webkit-slider-thumb {
+    width: ${props => props.thumbWidth};
+    height: ${props => props.thumbHeight};
     appearance: none;
-    height: 8px;
-    cursor: ew-resize;
-    background: ${props => props.theme.colors.primaryText};
-    box-shadow: -1000px 0 0 1000px orange;
+    background: ${props => props.theme.colors[props.thumbBackground]};
+    border-radius: ${props => props.thumbRadius};
+    box-shadow: ${props =>
+      makelongshadow(
+        props.theme.colors[props.backgroundSlider],
+        props.shadowSize,
+        props.sliderWidthNumber
+      )};
+    margin-top: ${props => props.fitThumbInSlider};
+    border: ${props => props.thumbBorder};
   }
 `
 
 const Range = asField(({ fieldState, fieldApi, ...props }) => {
   const { value } = fieldState
   const { setValue, setTouched } = fieldApi
-  const { onChange, onBlur, forwardedRef, ...rest } = props
+  const { onChange, onBlur, forwardedRef, sliderWidthNumber = 200, ...rest } = props
+  const styleProps = {
+    sliderWidthNumber,
+    sliderWidth: `${sliderWidthNumber}px`,
+    sliderHeight: '4px',
+    sliderBorderWidth: '0px',
+    sliderBorderColor: 'white',
+    sliderBorderRadius: '3px',
+    backgroundSlider: 'gray',
+    thumbWidth: '8px',
+    thumbHeight: '8px',
+    thumbRadius: '50%',
+    thumbBorder: '0px solid black',
+    thumbBackground: 'primaryText',
+    shadowSize: '-2px',
+    fitThumbInSlider: '-2px',
+    backgroundFilledSlider: 'lightningOrange'
+  }
+
   return (
-    <Input
-      min={0}
-      max={100}
-      step={1}
-      {...rest}
-      type="range"
-      ref={forwardedRef}
-      value={value || 0}
-      onChange={e => {
-        setValue(e.target.value)
-        if (onChange) {
-          onChange(e)
-        }
-      }}
-      onBlur={e => {
-        setTouched()
-        if (onBlur) {
-          onBlur(e)
-        }
-      }}
-    />
+    <>
+      <Input
+        {...styleProps}
+        min={0}
+        max={100}
+        step={1}
+        {...rest}
+        type="range"
+        ref={forwardedRef}
+        value={value || 0}
+        onChange={e => {
+          setValue(e.target.value)
+          if (onChange) {
+            onChange(e)
+          }
+        }}
+        onBlur={e => {
+          setTouched()
+          if (onBlur) {
+            onBlur(e)
+          }
+        }}
+      />
+    </>
   )
 })
 
