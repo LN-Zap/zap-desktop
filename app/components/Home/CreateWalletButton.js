004218e18af8fa42ffb6023462515a8626f7e8c6
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2018-11-26 15:38:41 +0100

    chore(i18n): i18n support for Home components

--- app/components/Home/CreateWalletButton.js
+++ app/components/Home/CreateWalletButton.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { FormattedMessage } from 'react-intl'
 import { Flex } from 'rebass'
 import { Button, Text } from 'components/UI'
 import PlusCircle from 'components/Icon/PlusCircle'
+import messages from './messages'
 
 const CreateWalletButton = ({ ...rest }) => (
   <Button {...rest} variant="secondary">
@@ -10,7 +12,7 @@ const CreateWalletButton = ({ ...rest }) => (
         <PlusCircle width="22px" height="22px" />
       </Text>
       <Text lineHeight="22px" ml={2}>
-        Create new wallet
+        <FormattedMessage {...messages.create_wallet_button_text} />
       </Text>
     </Flex>
   </Button>
---
+++ app/components/Home/messages.js
@@ -0,0 +1,26 @@
+import { defineMessages } from 'react-intl'
+
+/* eslint-disable max-len */
+export default defineMessages({
+  create_wallet_button_text: 'Create new wallet',
+  launch_wallet_button_text: 'Launch now',
+  settings_title: 'Settings',
+  delete_title: 'Delete',
+  delete_wallet_button_text: 'Delete wallet',
+  wallet_settings_name_label: 'Wallet name',
+  wallet_settings_name_description: 'The wallet name is only visible for you inside Zap.',
+  wallet_settings_name_placeholder: 'Enter name',
+  wallet_settings_alias_label: 'Alias',
+  wallet_settings_alias_description: 'The alias will be visible for others on the network.',
+  wallet_settings_alias_placeholder: 'Enter alias',
+  wallet_settings_autopilotAllocation_label: 'Percentage of Balance',
+  wallet_settings_autopilotMaxchannels_label: 'Number of Channels max',
+  wallet_settings_autopilotMinchansize_label: 'Minimum channel size',
+  wallet_settings_autopilotMaxchansize_label: 'Maximum channel size',
+  wallet_settings_reset_autopilot_button_text: 'Set Autopilot settings to default',
+  wallets_menu_local_title: 'Your wallets',
+  wallets_menu_other_title: 'More',
+  wallet_unlocker_password_label: 'Enter Password',
+  wallet_unlocker_password_placeholder: 'Enter your password',
+  wallet_unlocker_button_label: 'Enter'
+})
