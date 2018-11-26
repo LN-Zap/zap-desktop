c51c0e77890140994c1de39328edf44a5f78a2ea
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2018-11-26 12:21:31 +0100

    style(home): new layout for wallet settings form

--- app/components/UI/DataRow.js
+++ app/components/UI/DataRow.js
@@ -5,12 +5,8 @@ import { Text } from 'components/UI'
 
 const DataRow = ({ left, right, ...rest }) => (
   <Flex alignItems="center" py={3} {...rest} justifyContent="space-between">
-    <Text width={1 / 2} fontWeight="normal">
-      {left}
-    </Text>
-    <Text width={1 / 2} textAlign="right">
-      {right}
-    </Text>
+    <Text fontWeight="normal">{left}</Text>
+    <Text textAlign="right">{right}</Text>
   </Flex>
 )
 
