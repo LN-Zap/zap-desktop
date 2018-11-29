0bf25c9785fa27b0a4c8ff62c6e8f5f256bd9c45
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2018-11-29 12:59:51 +0100

    feat(home): implement No Wallets screen

---
+++ app/components/Home/NoWallets.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { FormattedMessage } from 'react-intl'
+import { Flex } from 'rebass'
+import { Heading } from 'components/UI'
+import { CreateWalletButton } from '.'
+import messages from './messages'
+
+const NoWallets = ({ history, wallets }) => (
+  <Flex flexDirection="column" justifyContent="center" alignItems="center" css={{ height: '100%' }}>
+    {wallets.length === 0 ? (
+      <>
+        <Heading.h4>
+          <FormattedMessage {...messages.no_wallets_message} />
+        </Heading.h4>
+      </>
+    ) : (
+      <>
+        <Heading.h4>
+          <FormattedMessage {...messages.no_active_wallet_message} />
+        </Heading.h4>
+      </>
+    )}
+    <Heading.h4 mt={2}>
+      <FormattedMessage {...messages.create_wallet_promot} />
+    </Heading.h4>
+    <CreateWalletButton history={history} p={3} />
+  </Flex>
+)
+NoWallets.propTypes = {
+  wallets: PropTypes.array.isRequired,
+  history: PropTypes.object.isRequired
+}
+
+export default NoWallets
