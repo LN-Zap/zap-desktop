18402aa3ddaf36d8c08df6714fb9c1866a560af6
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2018-12-10 21:32:13 +0100

    chore(i18n): extract new language strings

--- app/components/Activity/Payment/messages.js
+++ app/components/Activity/Payment/messages.js
@@ -4,5 +4,8 @@ import { defineMessages } from 'react-intl'
 export default defineMessages({
   amount: 'Payment amount',
   fee: 'Payment fee',
-  type: 'Lightning payment'
+  type: 'Lightning payment',
+  status_processing: 'Zap is processing your payment. This can take up to 01:00 min.',
+  status_success: 'Your payment was successful.',
+  status_error: 'There was a problem sending your payment.'
 })
