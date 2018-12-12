d3444251282762db3d9ea6cfef6dc32b0bdacd40
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2018-12-12 15:54:55 +0100

    feat(network): fetch node data on backoff schedule
    Refetch node and network info on an exponential backoff schedule to
    ensure that node data is available when users need it.
    
    This starts off fetching every 2 seconds and increases by a factor of
    1.5 until a maximum of a 10 minute refetch is reached where it remains
    until the app is unmounted.

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -11,7 +11,21 @@ import ActivityModal from 'components/Activity/ActivityModal'
 import Activity from 'containers/Activity'
 import { MainContent, Sidebar } from 'components/UI'
 
+// Initial refetch after 2 seconds.
+const INITIAL_REFETCH_INTERVAL = 2000
+
+// Fetch node data no less than once every 10 minutes.
+const MAX_REFETCH_INTERVAL = 1000 * 60 * 10
+
+// Amount to increment refetch timer by after each fetch.
+const BACKOFF_SCHEDULE = 1.5
+
 class App extends React.Component {
+  state = {
+    timer: null,
+    nextFetchIn: INITIAL_REFETCH_INTERVAL
+  }
+
   static propTypes = {
     form: PropTypes.object.isRequired,
     formProps: PropTypes.object.isRequired,
@@ -30,17 +44,44 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    const { fetchInfo, fetchDescribeNetwork, fetchPeers, setIsWalletOpen } = this.props
+    const { setIsWalletOpen } = this.props
 
     // Set wallet open state.
     setIsWalletOpen(true)
 
     // fetch node info.
-    fetchInfo()
+    this.fetchData()
+  }
 
+  componentWillUnmount() {
+    const { timer } = this.state
+    clearTimeout(timer)
+  }
+
+  /**
+   * Fetch node data on an expontially incrementing backoff schedule so that when the app is first mounted, we fetch
+   * node data quite frequently but as time goes on the frequency is reduced down to a maximum of MAX_REFETCH_INTERVAL
+   */
+  fetchData = () => {
+    const { nextFetchIn } = this.state
+    const next = Math.round(Math.min(nextFetchIn * BACKOFF_SCHEDULE, MAX_REFETCH_INTERVAL))
+
+    // Fetch data.
+    this.initNode()
+
+    // Schedule next fetch.
+    const timer = setTimeout(this.fetchData, nextFetchIn)
+
+    // Increment the next fetch interval.
+    this.setState({ timer, nextFetchIn: next })
+  }
+
+  initNode = () => {
+    const { fetchInfo, fetchDescribeNetwork, fetchPeers } = this.props
+    // fetch node info.
+    fetchInfo()
     // fetch LN network from nodes POV.
     fetchDescribeNetwork()
-
     // Fetch information about connected peers.
     fetchPeers()
   }
