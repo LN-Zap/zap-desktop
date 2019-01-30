8012f1d41b1b03fb11b71f65bce52554ec79f7b8
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-01-30 18:10:05 +0100

    fix(wallet): prevent crash on currency conversions
    Ensure that strings are coerced into numbers in the Value component
    prior to formatting them for output. Also, use FormattedNumber to
    output properly formatted numbers.
    
    Fix #1452

--- app/components/UI/Value.js
+++ app/components/UI/Value.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { FormattedNumber } from 'react-intl'
 import { convert } from 'lib/utils/btc'
 
-const forcePositive = amount => (amount >= 0 ? amount : amount * -1)
+const forcePositiveNumber = amount => (amount >= 0 ? amount * 1 : amount * -1)
 
 /**
  * Renders a satoshi amount into a specific currency/unit.
@@ -11,29 +12,36 @@ const forcePositive = amount => (amount >= 0 ? amount : amount * -1)
  * @param {[type]} currentTicker Current fiat ticker info.
  * @param {[type]} fiatTicker    Key of fiat ticker to use for fiat conversions.
  */
-const Value = ({ value, currency, currentTicker, fiatTicker }) => {
+const Value = ({ value, currency, currentTicker, fiatTicker, style }) => {
   let price
   if (currency === 'fiat') {
     price = currentTicker[fiatTicker]
   }
 
   // Convert the satoshi amount to the requested currency
-  const convertedAmount = convert('sats', currency, forcePositive(value), price)
+  const convertedAmount = convert('sats', currency, forcePositiveNumber(value), price)
 
   // Truncate the amount to the most relevant number of decimal places:
   let dp = 11
   switch (currency) {
     case 'btc':
+    case 'ltc':
       dp = 8
       break
     case 'bits':
+    case 'phots':
       dp = 5
       break
     case 'sats':
+    case 'lits':
       dp = 3
       break
     case 'msats':
+    case 'mlits':
       dp = 0
+      break
+    case 'fiat':
+      dp = 2
       break
   }
 
@@ -41,14 +49,26 @@ const Value = ({ value, currency, currentTicker, fiatTicker }) => {
 
   // Convert to a string and remove all trailing zeros.
   const trimmedAmount = String(truncatedAmount).replace(/\.?0+$/, '')
-  return <span>{trimmedAmount}</span>
+  return (
+    <FormattedNumber
+      value={trimmedAmount}
+      maximumFractionDigits={dp}
+      style={style}
+      currency={fiatTicker}
+    />
+  )
 }
 
 Value.propTypes = {
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   currency: PropTypes.string.isRequired,
   currentTicker: PropTypes.object,
-  fiatTicker: PropTypes.string
+  fiatTicker: PropTypes.string,
+  style: PropTypes.oneOf(['decimal', 'currency'])
+}
+
+Value.defaultProps = {
+  style: 'decimal'
 }
 
 export default Value
