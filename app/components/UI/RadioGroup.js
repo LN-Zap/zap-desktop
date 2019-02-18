045fa250bc3c3a74441d624f9e8c82cad76f3eeb
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-02-18 19:32:20 +0100

    refactor(ui): allow label and description for RadioGroup

--- app/components/UI/RadioGroup.js
+++ app/components/UI/RadioGroup.js
@@ -1,3 +1,40 @@
-import { RadioGroup } from 'informed'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { RadioGroup as InformedRadioGroup } from 'informed'
+import { Label, Span, Text } from 'components/UI'
+
+const RadioGroup = ({ label, field, description, required, children, ...rest }) => (
+  <>
+    {label && (
+      <Label htmlFor={field} mb={2}>
+        {label}
+        {required && (
+          <Span fontSize="s" css={{ 'vertical-align': 'super' }}>
+            {' '}
+            *
+          </Span>
+        )}
+      </Label>
+    )}
+
+    <InformedRadioGroup field={field} required={required} {...rest}>
+      {children}
+    </InformedRadioGroup>
+
+    {description && (
+      <Text color="gray" fontSize="s" mt={2}>
+        {description}
+      </Text>
+    )}
+  </>
+)
+
+RadioGroup.propTypes = {
+  description: PropTypes.node,
+  label: PropTypes.node,
+  field: PropTypes.string.isRequired,
+  required: PropTypes.bool,
+  children: PropTypes.node
+}
 
 export default RadioGroup
