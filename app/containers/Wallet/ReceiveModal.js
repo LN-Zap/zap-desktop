bd9a8a488246de512f78b028a7ffd70948151d49
Alexey Padalko <korhaliv@protonmail.com>
2019-02-21 10:41:21 +0200

    refactor: create receive modal container

---
+++ app/containers/Wallet/ReceiveModal.js
@@ -0,0 +1,28 @@
+import { connect } from 'react-redux'
+import get from 'lodash.get'
+import ReceiveModal from 'components/Wallet/ReceiveModal'
+import { walletSelectors } from 'reducers/wallet'
+import { tickerSelectors } from 'reducers/ticker'
+import { infoSelectors } from 'reducers/info'
+import { showNotification } from 'reducers/notification'
+import { closeWalletModal } from 'reducers/address'
+
+const mapStateToProps = state => ({
+  isOpen: state.address.walletModal,
+  networkInfo: infoSelectors.networkInfo(state),
+  cryptoName: tickerSelectors.currencyName(state),
+  pubkey: get(state.info, 'data.uris[0]') || get(state.info, 'data.identity_pubkey'),
+  address: state.address.address,
+  activeWalletSettings: walletSelectors.activeWalletSettings(state),
+  alias: state.info.data.alias
+})
+
+const mapDispatchToProps = {
+  onClose: closeWalletModal,
+  showNotification
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ReceiveModal)
