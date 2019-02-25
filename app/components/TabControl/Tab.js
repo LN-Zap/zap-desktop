e3ab531155a338dd9b9545fdc788f20fe081bfac
Alexey Padalko <korhaliv@protonmail.com>
2019-02-25 22:38:08 +0200

    refactor: introduce persistent tab control component

---
+++ app/components/TabControl/Tab.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// don't update a tab if it's currently hidden
+const shouldUpdate = (prevProps, nextProps) => !nextProps.isActive
+
+const Tab = React.memo(({ children }) => <>{children}</>, shouldUpdate)
+Tab.displayName = 'Tab'
+
+Tab.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+export default Tab
