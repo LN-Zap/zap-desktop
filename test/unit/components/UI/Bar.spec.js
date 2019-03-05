20a31148a44adb08fa669f3db490a4d704371c6a
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-03-05 13:11:07 +0100

    test(unit): update test snapshots

--- test/unit/components/UI/Bar.spec.js
+++ test/unit/components/UI/Bar.spec.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { ThemeProvider } from 'styled-components'
+import { dark } from 'themes'
 import { Bar } from 'components/UI'
 
 describe('component.UI.Bar', () => {
   it('should render correctly', () => {
-    const tree = renderer.create(<Bar />).toJSON()
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={dark}>
+          <Bar />
+        </ThemeProvider>
+      )
+      .toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
