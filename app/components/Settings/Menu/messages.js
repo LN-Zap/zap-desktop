1962074e796cb564832ea2f3bf9a77bb97492b54
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-03-13 12:32:06 +0100

    feat(autopay): add autopay link into wallet menu

--- app/components/Settings/Menu/messages.js
+++ app/components/Settings/Menu/messages.js
@@ -2,6 +2,7 @@ import { defineMessages } from 'react-intl'
 
 /* eslint-disable max-len */
 export default defineMessages({
+  autopay: 'Autopay',
   fiat: 'Fiat Currency',
   locale: 'Language',
   theme: 'Theme',
