5e1aaee6771b36f8d56599f5b68abd9e405be9eb
Alexey Padalko <korhaliv@protonmail.com>
2019-03-25 18:42:28 +0200

    refactor(autopay): extract AutopayGrid component

---
+++ app/components/Autopay/AutopayGrid.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Box } from 'rebass'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import AutopayCardView from './AutopayCardView'
+
+const Grid = styled(Box)`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 195px);
+  grid-gap: 1rem;
+`
+
+const AutopayGrid = ({ items, onClick }) => (
+  <Grid>
+    {items.map(item => {
+      return (
+        <Box key={item.pubkey} py={2}>
+          <AutopayCardView merchant={item} onClick={onClick} />
+        </Box>
+      )
+    })}
+  </Grid>
+)
+
+AutopayGrid.propTypes = {
+  items: PropTypes.array,
+  onClick: PropTypes.func.isRequired,
+}
+
+export default AutopayGrid
