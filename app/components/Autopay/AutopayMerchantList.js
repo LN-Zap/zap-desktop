a94d46a2b2efa60b242aea6f8467068e67353d8c
Alexey Padalko <korhaliv@protonmail.com>
2019-03-25 23:50:20 +0200

    refactor: extract AutopayMerchantList comp

---
+++ app/components/Autopay/AutopayMerchantList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Box } from 'rebass'
+import AutopayGrid from './AutopayGrid'
+import AutopaySearchNoResults from './AutopaySearchNoResults'
+
+const AutopayList = ({ merchants, openAutopayCreateModal, ...rest }) => {
+  if (merchants.length === 0) {
+    return <AutopaySearchNoResults {...rest} />
+  }
+
+  return (
+    <Box as="article" {...rest}>
+      <AutopayGrid items={merchants} onClick={openAutopayCreateModal} />
+    </Box>
+  )
+}
+
+AutopayList.propTypes = {
+  merchants: PropTypes.array,
+  openAutopayCreateModal: PropTypes.func.isRequired,
+}
+
+AutopayList.defaultProps = {
+  merchants: [],
+}
+
+export default AutopayList
---
+++ app/containers/Autopay/AutopayMerchantList.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux'
+import AutopayMerchantList from 'components/Autopay/AutopayMerchantList'
+import { openAutopayCreateModal, autopaySelectors } from 'reducers/autopay'
+
+const mapStateToProps = state => ({
+  merchants: autopaySelectors.filteredMerchants(state),
+})
+
+const mapDispatchToProps = {
+  openAutopayCreateModal,
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AutopayMerchantList)
