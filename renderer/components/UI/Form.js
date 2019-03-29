06aede8215d9adabb62e86b7c7709b99f8c70777
Alexey Padalko <korhaliv@protonmail.com>
2019-03-29 22:27:28 +0200

    refactor(form): ability to add async validators

--- renderer/components/UI/Form.js
+++ renderer/components/UI/Form.js
@@ -1,13 +1,56 @@
+import React, { useRef } from 'react'
+import PropTypes from 'prop-types'
 import system from '@rebass/components'
 import { styles } from 'styled-system'
 import { Form as InformedForm } from 'informed'
 
 // Create an html input element that accepts all style props from styled-system.
-const Form = system(
+const FormInner = system(
   {
     extend: InformedForm,
   },
   ...Object.keys(styles)
 )
+
+const Form = ({ asyncValidators, onSubmit, onSubmitFailure, getApi, ...rest }) => {
+  const formApi = useRef(null)
+
+  const onBeforeSubmit = async values => {
+    // pause submit if we have async validators set
+    if (asyncValidators && asyncValidators.length) {
+      await Promise.all(asyncValidators.map(v => v()))
+    }
+
+    const api = formApi.current
+    if (api) {
+      if (api.getState().invalid) {
+        onSubmitFailure && onSubmitFailure()
+      } else {
+        onSubmit && onSubmit(values)
+      }
+    }
+  }
+
+  const getApiWrapper = api => {
+    formApi.current = api
+    getApi && getApi(api)
+  }
+
+  return (
+    <FormInner
+      {...rest}
+      getApi={getApiWrapper}
+      onSubmit={onBeforeSubmit}
+      onSubmitFailure={onSubmitFailure}
+    />
+  )
+}
+
+Form.propTypes = {
+  asyncValidators: PropTypes.array,
+  getApi: PropTypes.func,
+  onSubmit: PropTypes.func,
+  onSubmitFailure: PropTypes.func,
+}
 
 export default Form
