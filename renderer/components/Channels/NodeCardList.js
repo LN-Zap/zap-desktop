be9b57161a54f8e729542392e9d483cfebad3d3c
Alexey Padalko <korhaliv@protonmail.com>
2019-03-31 21:13:08 +0300

    fix(ui): hide suggested nodes channel when empty
    resolve #1918

--- renderer/components/Channels/NodeCardList.js
+++ renderer/components/Channels/NodeCardList.js
@@ -8,29 +8,33 @@ import messages from './messages'
 
 const NodeCardList = ({ nodes, updateContactFormSearchQuery, ...rest }) => (
   <Box as="article" {...rest}>
-    <Heading.h1 mb={3}>
-      <FormattedMessage {...messages.node_suggestions_title} />
-    </Heading.h1>
-    <Flex flexWrap="wrap">
-      {nodes.map((nodeInfo, index) => {
-        const column = index % 3
-        const isLeft = column === 0
-        const isMiddle = column === 1
-        const isRight = column === 2
+    {Boolean(nodes && nodes.length) && (
+      <>
+        <Heading.h1 mb={3}>
+          <FormattedMessage {...messages.node_suggestions_title} />
+        </Heading.h1>
+        <Flex flexWrap="wrap">
+          {nodes.map((nodeInfo, index) => {
+            const column = index % 3
+            const isLeft = column === 0
+            const isMiddle = column === 1
+            const isRight = column === 2
 
-        return (
-          <Flex
-            key={nodeInfo.pubkey}
-            pb={4}
-            pl={isLeft ? 0 : isMiddle ? 3 : 4}
-            pr={isRight ? 0 : isMiddle ? 3 : 4}
-            width={1 / 3}
-          >
-            <NodeCardView {...nodeInfo} nodeClicked={updateContactFormSearchQuery} width={1} />
-          </Flex>
-        )
-      })}
-    </Flex>
+            return (
+              <Flex
+                key={nodeInfo.pubkey}
+                pb={4}
+                pl={isLeft ? 0 : isMiddle ? 3 : 4}
+                pr={isRight ? 0 : isMiddle ? 3 : 4}
+                width={1 / 3}
+              >
+                <NodeCardView {...nodeInfo} nodeClicked={updateContactFormSearchQuery} width={1} />
+              </Flex>
+            )
+          })}
+        </Flex>
+      </>
+    )}
   </Box>
 )
 
