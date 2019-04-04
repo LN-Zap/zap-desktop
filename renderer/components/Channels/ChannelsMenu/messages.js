c8ddb486e421aecde43a290e294a703a22837a18
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-04-04 10:51:32 +0200

    refactor(channels): add ChannelsMenu component

---
+++ renderer/components/Channels/ChannelsMenu/messages.js
@@ -0,0 +1,13 @@
+import { defineMessages } from 'react-intl'
+
+/* eslint-disable max-len */
+export default defineMessages({
+  title: 'Channels',
+  menu_item_channels_title: 'Manage Channels',
+  menu_item_channels_description: 'Subtitle',
+  menu_item_channel_create_title: 'Create Channel',
+  menu_item_channel_create_description: 'Subtitle',
+  summary_row_lightning_title: 'Lightning',
+  summary_row_pending_title: 'Pending',
+  summary_row_onchain_title: 'On Chain',
+})
