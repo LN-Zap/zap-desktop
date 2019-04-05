1140682e4360dbf5580e71cd9624db1e86ca57dd
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-04-05 10:42:44 +0200

    docs(storybook): document Donut component

---
+++ stories/components/donut.stories.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { Donut } from 'components/UI'
+
+storiesOf('Components', module).addWithChapters('Donut', {
+  subtitle: 'For visualizing data.',
+  info: `The Donut component is used to visualize a data series in a dunut chart.`,
+  chapters: [
+    {
+      sections: [
+        {
+          title: 'Empty',
+          sectionFn: () => <Donut height={200} width={200} />,
+        },
+        {
+          title: 'Basic',
+          sectionFn: () => (
+            <Donut
+              data={[
+                {
+                  key: 'lightning',
+                  amount: 0.5,
+                  color: 'lightningOrange',
+                  withGlow: true,
+                  withTint: true,
+                },
+              ]}
+              height={200}
+              width={200}
+            />
+          ),
+        },
+        {
+          title: 'With text',
+          sectionFn: () => (
+            <Donut
+              data={[
+                {
+                  key: 'lightning',
+                  amount: 0.25,
+                  color: 'lightningOrange',
+                  withGlow: true,
+                  withTint: true,
+                },
+                {
+                  key: 'pending',
+                  amount: 0.4,
+                  color: 'gray',
+                  withGlow: false,
+                  withTint: false,
+                },
+                {
+                  key: 'onchain',
+                  amount: 0.35,
+                  color: 'secondaryColor',
+                  withGlow: false,
+                  withTint: true,
+                },
+              ]}
+              height={200}
+              text={10}
+              width={200}
+            />
+          ),
+        },
+      ],
+    },
+  ],
+})
