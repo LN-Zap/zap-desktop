2a9c7825cc05e52eb7b065ee8b8e06f809d42184
Alexey Padalko <korhaliv@protonmail.com>
2019-04-15 14:45:59 +0300

    refactor(hooks): fix useCloseOnUnmount deps issues

--- renderer/hooks/useCloseOnUnmount.js
+++ renderer/hooks/useCloseOnUnmount.js
@@ -1,13 +1,20 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 /**
  * React hook to be used with modals and dialog boxes
  * calls @onClose  when the component unmounts
  * This is useful to ensure dialog is always closed even in case unmounted together with parent
  */
 export default function useCloseOnUnmount(isOpen, onClose) {
+  const savedCallback = useRef()
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = onClose
+  })
+
   useEffect(() => {
     return () => {
-      !isOpen && onClose()
+      savedCallback.current()
     }
   }, [])
 }
