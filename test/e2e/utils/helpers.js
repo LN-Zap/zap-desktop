759410159b71e2804e1add6bcdde3bb2a2cbca97
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-04-20 14:05:43 +0200

    test(e2e): debug lnd processes

--- test/e2e/utils/helpers.js
+++ test/e2e/utils/helpers.js
@@ -1,7 +1,12 @@
 import { ClientFunction } from 'testcafe'
 import path from 'path'
 import rimraf from 'rimraf'
+import ps from 'ps-node'
+import { promisify } from 'util'
 import delay from '../../../utils/delay'
+
+const rimrafPromise = promisify(rimraf)
+const psLookup = promisify(ps.lookup)
 
 // Get the path to the index page.
 export const getBaseUrl = () => '../../dist/index.html'
@@ -19,6 +24,7 @@ export const killLnd = ClientFunction(() => window.Zap.killLnd())
 export const deleteDatabase = ClientFunction(() => {
   // Catch unhandled errors, which can happen if an attempt to access the database is made after we have closed it.
   window.addEventListener('unhandledrejection', event => {
+    console.warn(`UNHANDLED PROMISE REJECTION: ${event.reason}`)
     event.preventDefault()
   })
   return window.db.delete()
@@ -30,18 +36,35 @@ export const assertNoConsoleErrors = async t => {
   await t.expect(error).eql([])
 }
 
+const printLndProcesses = async () => {
+  const processes = await psLookup({ command: 'lnd' })
+  console.log('lnd processes', processes)
+}
+
 // Clean out test environment.
 export const cleanTestEnvironment = async () => {
   console.log('cleanTestEnvironment')
   console.log('waiting 3 seconds for app state to settle')
   await delay(3000)
+
+  await printLndProcesses()
+
+  console.log('killing lnd')
   await killLnd()
+  console.log('lnd killed')
+
+  await printLndProcesses()
+
+  console.log('deleting database')
   await deleteDatabase()
+  console.log('database deleted')
 }
 
 // Clean out test environment.
 export const cleanElectronEnvironment = async ctx => {
+  console.log('cleanElectronEnvironment')
   if (ctx.userDataDir) {
-    rimraf.sync(path.join(ctx.userDataDir, 'lnd'), { disableGlob: false, maxBusyTries: 10 })
+    await rimrafPromise(path.join(ctx.userDataDir, 'lnd'), { disableGlob: false, maxBusyTries: 10 })
+    console.log('env cleaned')
   }
 }
