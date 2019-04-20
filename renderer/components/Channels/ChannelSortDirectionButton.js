9e59e678f3757449143ace854edb37e271ed3973
Alexey Padalko <korhaliv@protonmail.com>
2019-04-20 20:29:00 +0300

    refactor(channels): add ChannelSortDirectionButton component

---
+++ renderer/components/Channels/ChannelSortDirectionButton.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { injectIntl } from 'react-intl'
+import { SwitchButton } from 'components/UI'
+import ArrowUp from 'components/Icon/ArrowUp'
+import ArrowDown from 'components/Icon/ArrowDown'
+import messages from './messages'
+
+const ChannelSortDirectionButton = injectIntl(({ onClick, isAsc, intl, ...rest }) => {
+  return (
+    <SwitchButton
+      {...rest}
+      data-hint={intl.formatMessage({
+        ...(isAsc ? messages.channel_desc_sort : messages.channel_asc_sort),
+      })}
+      Icon1={ArrowUp}
+      Icon2={ArrowDown}
+      isSwitched={isAsc}
+      onClick={onClick}
+    />
+  )
+})
+
+ChannelSortDirectionButton.propTypes = {
+  isAsc: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+}
+
+export default ChannelSortDirectionButton
