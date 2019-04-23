b355ee0a71cdc9b60020f347f7174144fa8816ac
Alexey Padalko <korhaliv@protonmail.com>
2019-04-23 11:07:19 +0300

    refactor(i18n): add intl support for ChannelFilter
    resolve #2073

--- renderer/components/Channels/ChannelFilter.js
+++ renderer/components/Channels/ChannelFilter.js
@@ -1,30 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Form, Select } from 'components/UI'
+import messages from './messages'
 
-const ChannelFilter = ({ changeFilter, filter, filters, ...rest }) => {
-  // Reformat channel filters the way that Select element expects them to be.
-  const items = filters.map(f => {
-    return {
-      key: f.key,
-      value: f.name,
-    }
-  })
+const messageMapper = key => {
+  const filters = {
+    ALL_CHANNELS: messages.channel_filter_all,
+    ACTIVE_CHANNELS: messages.channel_filter_online,
+    NON_ACTIVE_CHANNELS: messages.channel_filter_offline,
+    OPEN_PENDING_CHANNELS: messages.channel_filter_pending,
+    CLOSING_PENDING_CHANNELS: messages.channel_filter_closing,
+  }
 
-  return (
-    <Form {...rest}>
-      <Select
-        field="channel-filter"
-        highlightOnValid={false}
-        id="channel-filter"
-        initialSelectedItem={filter}
-        items={items}
-        onValueSelected={changeFilter}
-        width={1}
-      />
-    </Form>
-  )
+  return filters[key]
 }
+
+const ChannelFilter = ({ changeFilter, filter, filters, ...rest }) => (
+  <Form {...rest}>
+    <Select
+      field="channel-filter"
+      highlightOnValid={false}
+      id="channel-filter"
+      initialSelectedItem={filter}
+      items={filters}
+      messageMapper={messageMapper}
+      onValueSelected={changeFilter}
+      width={1}
+    />
+  </Form>
+)
 
 ChannelFilter.propTypes = {
   changeFilter: PropTypes.func.isRequired,
