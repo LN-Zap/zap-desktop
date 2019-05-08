8cebeab25adec7766542e30abd78f005e3ef21ca
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-05-08 10:12:42 +0200

    build(webpack): fix hot reload issue
    Run preload build script in a separate Webpack pipeline to prevent it
    breaking the hot-reload setup.
    
    Fix #2012

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -6,11 +6,10 @@ import CleanWebpackPlugin from 'clean-webpack-plugin'
 
 import mainConfig from './dev/main.config'
 import workersConfig from './dev/workers.config'
-import preloadConfig from './dev/preload.config'
 import rendererConfig from './dev/renderer.config'
 
 mainConfig.plugins.unshift(
-  new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ['**/*', '!config.json'] })
+  new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ['**/*', '!config.json', '!preload.js'] })
 )
 
-export default [mainConfig, workersConfig, preloadConfig, rendererConfig]
+export default [mainConfig, workersConfig, rendererConfig]
