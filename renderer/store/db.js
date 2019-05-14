7255275e292d6f872f4d74139f10deb500b0c92c
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-05-14 22:30:13 +0200

    refactor: migrate old settings to config overrides

--- renderer/store/db.js
+++ renderer/store/db.js
@@ -25,6 +25,7 @@ export const initDb = () => {
 export const getDb = name => {
   const db = new Dexie(name)
 
+  // Initial database schema.
   db.version(1).stores({
     settings: 'key',
     wallets: '++id, type, chain, network',
@@ -61,8 +62,81 @@ export const getDb = name => {
       delete wallet.string
     })
   )
+
+  // ADd autopay table.
   db.version(3).stores({
     autopay: 'id',
+  })
+
+  // Convert old settings to new config overrides
+  db.version(4).upgrade(async tx => {
+    const configMap = [
+      {
+        settingsKey: 'theme',
+        settingsPath: 'value',
+        configPath: 'theme',
+      },
+      {
+        settingsKey: 'locale',
+        settingsPath: 'value',
+        configPath: 'locale',
+      },
+      {
+        settingsKey: 'fiatTicker',
+        settingsPath: 'value',
+        configPath: 'currency',
+      },
+      {
+        settingsKey: 'channelViewMode',
+        settingsPath: 'value',
+        configPath: 'channels.viewMode',
+        configMutator: value => {
+          switch (value) {
+            case 'CHANNEL_LIST_VIEW_MODE_SUMMARY':
+              return 'summary'
+            case 'CHANNEL_LIST_VIEW_MODE_CARD':
+              return 'card'
+            default:
+              return
+          }
+        },
+      },
+      {
+        settingsKey: 'chain.bitcoin',
+        settingsPath: 'value.unit',
+        configPath: 'units.bitcoin',
+      },
+      {
+        settingsKey: 'chain.litecoin',
+        settingsPath: 'value.unit',
+        configPath: 'units.litecoin',
+      },
+    ]
+    const newConfig = {}
+    await tx.settings.toCollection().modify((setting, ref) => {
+      const c = configMap.find(c => c.settingsKey === setting.key)
+      if (c) {
+        try {
+          const defaultValue = Dexie.getByKeyPath(config, c.configPath)
+          let value = Dexie.getByKeyPath(setting, c.settingsPath)
+          // Apply value mapper if there is one.
+          if (c.configMutator) {
+            value = c.configMutator(value)
+          }
+          // If the value differes from the default, save it into the config.
+          if (value && value !== defaultValue) {
+            Dexie.setByKeyPath(newConfig, c.configPath, value)
+          }
+          // Finally, delete the old setting.
+          delete ref.value
+        } catch (e) {
+          // If there was a problem migrating the old setting just delete the setting altogether.
+          delete ref.value
+        }
+      }
+    })
+    // Save the migrated config overrides.
+    await tx.settings.put({ key: 'config', value: newConfig })
   })
 
   /**
