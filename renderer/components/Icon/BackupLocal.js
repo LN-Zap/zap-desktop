e8b44bf81ad2b9e96c57622011b49ab23ad5fb37
Alexey Padalko <korhaliv@protonmail.com>
2019-05-15 20:37:37 +0300

    chore(icons): add backup icons

---
+++ renderer/components/Icon/BackupLocal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SvgBackupLocal = props => (
+  <svg height="1em" viewBox="0 0 74 70" width="1em" {...props}>
+    <g fill="none" fillRule="evenodd">
+      <path d="M0 69h74v1H0z" fill="currentColor" />
+      <path d="M37.512 63L33 54l9 .012z" stroke="currentColor" />
+      <path d="M38 54.24h-1l.86-54h1z" fill="currentColor" />
+    </g>
+  </svg>
+)
+
+export default SvgBackupLocal
