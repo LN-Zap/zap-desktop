f1f0f436627ad867178bf9c0236a7b8ee83bd41c
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-05-23 09:34:07 +0200

    refactor: support forwardRef in Card component

--- renderer/components/UI/Card.js
+++ renderer/components/UI/Card.js
@@ -1,23 +1,21 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Card as BaseCard } from 'rebass'
 
-const Card = ({ children, ...rest }) => (
-  <BaseCard
-    as="section"
-    bg="primaryColor"
-    borderRadius={5}
-    boxShadow="0 2px 24px 0 rgba(0, 0, 0, 0.5)"
-    color="primaryText"
-    p={3}
-    {...rest}
-  >
-    {children}
-  </BaseCard>
-)
+const Card = React.forwardRef((props, ref) => {
+  return (
+    <BaseCard
+      ref={ref}
+      as="section"
+      bg="primaryColor"
+      borderRadius={5}
+      boxShadow="0 2px 24px 0 rgba(0, 0, 0, 0.5)"
+      color="primaryText"
+      p={3}
+      {...props}
+    />
+  )
+})
 
-Card.propTypes = {
-  children: PropTypes.node,
-}
+Card.displayName = 'Card'
 
 export default Card
