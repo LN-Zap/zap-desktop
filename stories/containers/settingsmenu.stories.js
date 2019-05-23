ab237095732a112b83e339361e53bfee42386b84
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-05-23 12:27:54 +0200

    docs(storybook): add SettingsMenu example

---
+++ stories/containers/settingsmenu.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import SettingsMenu from 'containers/Settings/SettingsMenu'
+import { Provider } from '../Provider'
+import { Window } from '../helpers'
+
+storiesOf('Containers.SettingsMenu', module)
+  .addParameters({ info: { disable: true } })
+  .addDecorator(story => <Window>{story()}</Window>)
+  .addDecorator(story => <Provider story={story()} />)
+  .add('SettingsMenu', () => <SettingsMenu activeWalletSettings={{ id: 1, type: 'local' }} />)
