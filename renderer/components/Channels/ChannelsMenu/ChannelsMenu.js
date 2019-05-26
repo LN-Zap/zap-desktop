1bb3b4efc84587aad2e374c407070244c30d2cb5
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-05-26 15:32:36 +0200

    refactor: update ChannelsMenu to use Dropmenu

--- renderer/components/Channels/ChannelsMenu/ChannelsMenu.js
+++ renderer/components/Channels/ChannelsMenu/ChannelsMenu.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Card } from 'components/UI'
+import { Dropmenu } from 'components/UI'
 import { FormattedMessage } from 'react-intl'
 import ChannelsMenuHeader from './ChannelsMenuHeader'
 import ChannelsMenuSummary from './ChannelsMenuSummary'
-import ChannelsMenuItem from './ChannelsMenuItem'
 import messages from './messages'
 
 const ChannelsMenu = ({
@@ -15,41 +14,60 @@ const ChannelsMenu = ({
   onchainBalance,
   currencyName,
   ...rest
-}) => (
-  <Card p={0} {...rest}>
-    <ChannelsMenuHeader
-      channelCount={channelCount}
-      lightningBalance={lightningBalance}
-      onchainBalance={onchainBalance}
-      p={3}
-      pendingBalance={pendingBalance}
-    />
-
-    <ChannelsMenuSummary
-      currencyName={currencyName}
-      lightningBalance={lightningBalance}
-      onchainBalance={onchainBalance}
-      pb={3}
-      pendingBalance={pendingBalance}
-      px={3}
-    />
-
-    <ChannelsMenuItem
-      description={<FormattedMessage {...messages.menu_item_channels_description} />}
-      onClick={() => openModal('CHANNELS')}
-      title={<FormattedMessage {...messages.menu_item_channels_title} />}
-    />
-
-    <ChannelsMenuItem
-      description={<FormattedMessage {...messages.menu_item_channel_create_description} />}
-      onClick={() => {
+}) => {
+  const items = [
+    {
+      type: 'content',
+      id: 'header',
+      content: (
+        <ChannelsMenuHeader
+          channelCount={channelCount}
+          lightningBalance={lightningBalance}
+          onchainBalance={onchainBalance}
+          p={2}
+          pendingBalance={pendingBalance}
+        />
+      ),
+    },
+    {
+      type: 'content',
+      id: 'summary',
+      content: (
+        <ChannelsMenuSummary
+          currencyName={currencyName}
+          lightningBalance={lightningBalance}
+          onchainBalance={onchainBalance}
+          pb={3}
+          pendingBalance={pendingBalance}
+          px={2}
+        />
+      ),
+    },
+    { id: 'bar1', type: 'bar' },
+    {
+      id: 'manage',
+      title: <FormattedMessage {...messages.menu_item_channels_title} />,
+      description: <FormattedMessage {...messages.menu_item_channels_description} />,
+      onClick: () => openModal('CHANNELS'),
+    },
+    { id: 'bar2', type: 'bar' },
+    {
+      id: 'create',
+      title: <FormattedMessage {...messages.menu_item_channel_create_title} />,
+      description: <FormattedMessage {...messages.menu_item_channel_create_description} />,
+      onClick: () => {
         openModal('CHANNELS')
         openModal('CHANNEL_CREATE')
-      }}
-      title={<FormattedMessage {...messages.menu_item_channel_create_title} />}
-    />
-  </Card>
-)
+      },
+    },
+  ]
+
+  return (
+    <Dropmenu items={items} justify="right" {...rest}>
+      Channels
+    </Dropmenu>
+  )
+}
 
 ChannelsMenu.propTypes = {
   channelCount: PropTypes.number.isRequired,
