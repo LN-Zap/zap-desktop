d7e4b799e94fa651a5b867aaa48bdb04b216f58b
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-06-01 10:42:17 +0200

    refactor: new ChannelCapacity components

---
+++ renderer/components/Channels/ChannelsCapacityDonut.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Box } from 'rebass'
+import { Donut } from 'components/UI'
+
+const ChannelsCapacityDonut = ({
+  lightningBalance,
+  channelCount,
+  onchainBalance,
+  pendingBalance,
+  ...rest
+}) => {
+  const total = lightningBalance + onchainBalance + pendingBalance
+  const lightningBalancePercent = lightningBalance / total
+  const onchainBalancePercent = onchainBalance / total
+  const pendingBalancePercent = pendingBalance / total
+
+  return (
+    <Box {...rest}>
+      <Donut
+        data={[
+          {
+            key: 'lightning',
+            amount: lightningBalancePercent || 0,
+            color: 'lightningOrange',
+            withGlow: true,
+            withTint: true,
+          },
+          {
+            key: 'pending',
+            amount: pendingBalancePercent || 0,
+            color: 'gray',
+            withTint: true,
+          },
+          {
+            key: 'onchain',
+            amount: onchainBalancePercent || 0,
+            color: 'secondaryColor',
+            withTint: true,
+          },
+        ]}
+        text={channelCount}
+      />
+    </Box>
+  )
+}
+
+ChannelsCapacityDonut.propTypes = {
+  channelCount: PropTypes.number.isRequired,
+  lightningBalance: PropTypes.number.isRequired,
+  onchainBalance: PropTypes.number.isRequired,
+  pendingBalance: PropTypes.number.isRequired,
+}
+
+export default ChannelsCapacityDonut
---
+++ renderer/components/Channels/ChannelsSummaryDonut.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Box } from 'rebass'
+import { Donut } from 'components/UI'
+
+const ChannelsSummaryDonut = ({ sendCapacity, receiveCapacity, ...rest }) => {
+  const total = sendCapacity + receiveCapacity
+  const sendCapacityPercent = sendCapacity / total
+  const receiveCapacityPercent = receiveCapacity / total
+
+  return (
+    <Box {...rest}>
+      <Donut
+        data={[
+          {
+            key: 'send',
+            amount: sendCapacityPercent || 0,
+            color: 'lightningOrange',
+            withGlow: true,
+            withTint: true,
+          },
+          {
+            key: 'reeive',
+            amount: receiveCapacityPercent || 0,
+            color: 'superBlue',
+            withTint: true,
+          },
+        ]}
+        strokeWidth={2}
+      />
+    </Box>
+  )
+}
+
+ChannelsSummaryDonut.propTypes = {
+  receiveCapacity: PropTypes.number.isRequired,
+  sendCapacity: PropTypes.number.isRequired,
+}
+
+export default ChannelsSummaryDonut
