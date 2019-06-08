03c0adcfcfae8fd888f905b830f3dcc3e3e2864b
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-06-08 13:53:22 +0200

    build(storybook): switch to webpack full-control mode

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,29 +1,28 @@
 require('@babel/register')
-
-const fs = require('fs')
-const path = require('path')
 const baseConfig = require('../webpack/webpack.config.base')
-const merge = require('webpack-merge')
-const config = require('config')
-const rootDir = path.join(__dirname, '..')
 
-module.exports = merge.smart(baseConfig, {
-  default: {
-    module: {
-      rules: [
-        {
-          test: /\.stories\.jsx?$/,
-          loaders: [require.resolve('@storybook/addon-storysource/loader')],
-          enforce: 'pre',
-        },
-      ],
-    },
-
-    node: {
-      __dirname: false,
-      __filename: false,
-      fs: 'empty',
-      module: 'empty',
-    },
+module.exports = ({ config }) => ({
+  ...config,
+  resolve: {
+    ...config.resolve,
+    ...baseConfig.default.resolve,
+  },
+  module: {
+    ...config.module,
+    rules: [
+      ...config.module.rules,
+      ...baseConfig.default.module.rules,
+      {
+        test: /\.stories\.jsx?$/,
+        loaders: [require.resolve('@storybook/addon-storysource/loader')],
+        enforce: 'pre',
+      },
+    ],
+  },
+  node: {
+    __dirname: false,
+    __filename: false,
+    fs: 'empty',
+    module: 'empty',
   },
 })
