93d615875c209f263f311d68feb499b2fe00b8af
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-06-11 20:45:09 +0200

    refactor: reduce ReceiveModal to only show address

--- renderer/containers/Wallet/ReceiveModal.js
+++ renderer/containers/Wallet/ReceiveModal.js
@@ -1,7 +1,6 @@
 import { connect } from 'react-redux'
-import get from 'lodash/get'
 import ReceiveModal from 'components/Wallet/ReceiveModal'
-import { addressSelectors, closeWalletModal } from 'reducers/address'
+import { addressSelectors } from 'reducers/address'
 import { walletSelectors } from 'reducers/wallet'
 import { tickerSelectors } from 'reducers/ticker'
 import { infoSelectors } from 'reducers/info'
@@ -9,15 +8,12 @@ import { showNotification } from 'reducers/notification'
 
 const mapStateToProps = state => ({
   networkInfo: infoSelectors.networkInfo(state),
-  chainName: tickerSelectors.cryptoAddressName(state),
-  pubkey: get(state.info, 'data.uris[0]') || get(state.info, 'data.identity_pubkey'),
-  address: addressSelectors.currentAddress(state),
+  cryptoAddressName: tickerSelectors.cryptoAddressName(state),
+  currentAddress: addressSelectors.currentAddress(state),
   activeWalletSettings: walletSelectors.activeWalletSettings(state),
-  alias: state.info.data.alias,
 })
 
 const mapDispatchToProps = {
-  onClose: closeWalletModal,
   showNotification,
 }
 
