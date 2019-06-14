0d6ace0ee9b65ca04c1d1376734d484037fec1d6
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-06-14 12:46:23 +0200

    feat(ui): implement static page header in profile page

--- renderer/components/Profile/ProfilePage/ProfilePage.js
+++ renderer/components/Profile/ProfilePage/ProfilePage.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { FormattedMessage } from 'react-intl'
-import { Heading, MainContent, Panel, Sidebar } from 'components/UI'
+import { Flex } from 'rebass'
+import { HeaderBar, MainContent, Panel, Sidebar } from 'components/UI'
 import ZapLogo from 'components/Icon/ZapLogo'
 import ProfilePaneConnect from 'containers/Profile/ProfilePaneConnect'
 import ProfilePaneNodeInfo from 'containers/Profile/ProfilePaneNodeInfo'
@@ -9,7 +10,7 @@ import ProfileMenu from '../ProfileMenu'
 import { PANE_NODEINFO, PANE_LNDCONNECT, DEFAULT_PANE } from '../constants'
 import messages from './messages'
 
-const ProfilePage = ({ activeWalletSettings }) => {
+const ProfilePage = ({ activeWalletSettings, ...rest }) => {
   const [group, setGroup] = useState(DEFAULT_PANE)
   const isLocalWallet = activeWalletSettings.type === 'local'
 
@@ -17,26 +18,35 @@ const ProfilePage = ({ activeWalletSettings }) => {
   const hasConnectPane = group === PANE_LNDCONNECT && !isLocalWallet
 
   return (
-    <>
+    <Flex width={1} {...rest}>
       <Sidebar.medium pt={40}>
         <Panel>
-          <Panel.Header mb={40} px={4}>
+          <Panel.Header mb={30} px={4}>
             <ZapLogo height={28} width={28} />
           </Panel.Header>
-          <Panel.Body css={{ 'overflow-y': 'overlay' }}>
+          <Panel.Body
+            css={`
+              overflow-y: overlay;
+            `}
+          >
             <ProfileMenu group={group} isLocalWallet={isLocalWallet} p={2} setGroup={setGroup} />
           </Panel.Body>
         </Panel>
       </Sidebar.medium>
-
-      <MainContent pb={2} pl={5} pr={6} pt={4}>
-        <Heading.h1 fontSize={60} mb={2}>
-          <FormattedMessage {...messages.profile_page_title} />
-        </Heading.h1>
-        {hasNodeInfoPane && <ProfilePaneNodeInfo />}
-        {hasConnectPane && <ProfilePaneConnect />}
+      <MainContent>
+        <Panel>
+          <Panel.Header>
+            <HeaderBar>
+              <FormattedMessage {...messages.profile_page_title} />
+            </HeaderBar>
+          </Panel.Header>
+          <Panel.Body css={{ 'overflow-y': 'overlay' }} mt={5} pb={2} px={5}>
+            {hasNodeInfoPane && <ProfilePaneNodeInfo />}
+            {hasConnectPane && <ProfilePaneConnect />}
+          </Panel.Body>
+        </Panel>
       </MainContent>
-    </>
+    </Flex>
   )
 }
 
