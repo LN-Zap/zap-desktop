389bb58e3a9b9d45a759e442c06426431fa02d50
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-06-17 09:23:47 +0200

    refactor: update CleanWebpackPlugin config

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -2,15 +2,15 @@
  * Webpack config for production builds
  */
 
-import CleanWebpackPlugin from 'clean-webpack-plugin'
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
 
 import mainConfig from './prod/main.config'
 import workersConfig from './prod/workers.config'
 import preloadConfig from './prod/preload.config'
 import rendererConfig from './prod/renderer.config'
 
-mainConfig.plugins.unshift(
+preloadConfig.plugins.unshift(
   new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ['**/*', '!config.json'] })
 )
 
-export default [mainConfig, workersConfig, preloadConfig, rendererConfig]
+export default [preloadConfig, workersConfig, mainConfig, rendererConfig]
