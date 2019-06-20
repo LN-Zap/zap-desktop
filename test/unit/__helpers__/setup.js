736a4458f50875cd3014c68306a998d7ddab1181
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-06-20 08:19:37 +0200

    test(unit): run preload to populate window with additions

--- test/unit/__helpers__/setup.js
+++ test/unit/__helpers__/setup.js
@@ -1,8 +1,9 @@
 import 'jest-styled-components'
-
 import { configure } from 'enzyme'
-
 import Adapter from 'enzyme-adapter-react-16'
+
+// Import the preload script in order to ensure that global helpers get injected.
+import '@zap/electron/preload'
 
 configure({ adapter: new Adapter() })
 
@@ -12,3 +13,5 @@ jest.mock('workers', () => ({
   neutrinoService: jest.fn(),
   grpcService: jest.fn(),
 }))
+
+jest.mock('dns')
