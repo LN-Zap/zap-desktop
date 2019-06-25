18f5a9d9734d8eb391d1ae23852be4fba8ac25c8
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-06-25 16:46:56 +0200

    refactor: remove duplicate logGrpcCmd method

--- utils/events.js
+++ utils/events.js
@@ -1,5 +1,3 @@
-import { grpcLog } from './log'
-
 /**
  * forwardEvent - Setup listener that re-emits specified event.
  *
@@ -32,14 +30,4 @@ export function forwardAll(source, baseEvent, target) {
 export function unforwardAll(source, baseEvent) {
   source.removeAllListeners(`${baseEvent}.data`)
   source.removeAllListeners(`${baseEvent}.error`)
-}
-
-/**
- * logGrpcCmd - Logs a service method invocation.
- *
- * @param {string} method Name of method to log
- * @param {any} payload Payload of method to log
- */
-export function logGrpcCmd(method, payload) {
-  grpcLog.info(`Calling ${method} with payload: %o`, payload)
 }
