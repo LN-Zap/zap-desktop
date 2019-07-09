aedaa698586cab086ac2e2ad2a6115d97a753813
Alexey Padalko <korhaliv@protonmail.com>
2019-07-09 23:27:35 +0200

    test: use initial state in address reducer test

--- test/unit/reducers/address.spec.js
+++ test/unit/reducers/address.spec.js
@@ -29,7 +29,7 @@ describe('reducers', () => {
           p2wkh: 'bc1q7l2z25nle43ngzzr86p2fwr2xlvtdkzm74e3tz',
         },
       }
-      expect(reducer({}, action)).toMatchSnapshot()
+      expect(reducer(undefined, action)).toMatchSnapshot()
     })
 
     it('should handle NEW_ADDRESS', () => {
@@ -37,7 +37,7 @@ describe('reducers', () => {
         type: NEW_ADDRESS,
         addressType: 'p2wkh',
       }
-      expect(reducer({}, action)).toMatchSnapshot()
+      expect(reducer(undefined, action)).toMatchSnapshot()
     })
 
     it('should handle NEW_ADDRESS_SUCCESS', () => {
@@ -46,7 +46,7 @@ describe('reducers', () => {
         addressType: 'np2wkh',
         address: '375WLnZzkEwKddQW22SGyhCfXNFz32Gvfc',
       }
-      expect(reducer({}, action)).toMatchSnapshot()
+      expect(reducer(undefined, action)).toMatchSnapshot()
     })
 
     it('should handle NEW_ADDRESS_FAILURE', () => {
@@ -55,21 +55,21 @@ describe('reducers', () => {
         addressType: 'np2wkh',
         error: 'some error',
       }
-      expect(reducer({}, action)).toMatchSnapshot()
+      expect(reducer(undefined, action)).toMatchSnapshot()
     })
 
     it('should handle OPEN_WALLET_MODAL', () => {
       const action = {
         type: OPEN_WALLET_MODAL,
       }
-      expect(reducer({}, action)).toMatchSnapshot()
+      expect(reducer(undefined, action)).toMatchSnapshot()
     })
 
     it('should handle CLOSE_WALLET_MODAL', () => {
       const action = {
         type: CLOSE_WALLET_MODAL,
       }
-      expect(reducer({}, action)).toMatchSnapshot()
+      expect(reducer(undefined, action)).toMatchSnapshot()
     })
   })
 })
