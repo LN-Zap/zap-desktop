cb7c69dafe0a962f16aad8b7d48edbc6cc344987
Alexey Padalko <korhaliv@protonmail.com>
2019-07-19 17:38:11 +0200

    fix: update channel search input if state changes

--- renderer/components/Channels/ChannelSearch.js
+++ renderer/components/Channels/ChannelSearch.js
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import { Form, Input } from 'components/UI'
 
-const ChannelSearch = ({ searchQuery, placeholder, updateChannelSearchQuery, ...rest }) => (
-  <Form {...rest}>
-    <Input
-      border={0}
-      field="channel-search"
-      highlightOnValid={false}
-      id="channel-search"
-      initialValue={searchQuery}
-      mr={2}
-      onValueChange={updateChannelSearchQuery}
-      placeholder={placeholder}
-      type="search"
-      variant="thin"
-    />
-  </Form>
-)
+const ChannelSearch = ({ searchQuery, placeholder, updateChannelSearchQuery, ...rest }) => {
+  const formApi = useRef(null)
+  const fieldId = 'channel-search'
+  const { current } = formApi
+  useEffect(() => {
+    // update input value if searchQuery has changed externally
+    if (current) {
+      current.setValue(fieldId, searchQuery)
+    }
+  }, [current, searchQuery])
+
+  return (
+    <Form
+      {...rest}
+      getApi={api => {
+        formApi.current = api
+      }}
+    >
+      <Input
+        border={0}
+        field={fieldId}
+        highlightOnValid={false}
+        id={fieldId}
+        initialValue={searchQuery}
+        mr={2}
+        onValueChange={updateChannelSearchQuery}
+        placeholder={placeholder}
+        type="search"
+        variant="thin"
+      />
+    </Form>
+  )
+}
 
 ChannelSearch.propTypes = {
   placeholder: PropTypes.string.isRequired,
