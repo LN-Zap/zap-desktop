d3a89f6b5771994967005c64ce80f2b3bc161a8d
Alexey Padalko <korhaliv@protonmail.com>
2019-07-22 23:21:11 +0300

    refactor: implement usePrevious hook

---
+++ renderer/hooks/usePrevious.js
@@ -0,0 +1,17 @@
+import { useEffect, useRef } from 'react'
+
+/**
+ * usePrevious - Caches specified value. This hooks is used to store previous prop
+ * value so it can be compared against the most recent one.
+ *
+ * @export
+ * @param {*} value next value
+ * @returns currently stored value
+ */
+export default function usePrevious(value) {
+  const ref = useRef()
+  useEffect(() => {
+    ref.current = value
+  })
+  return ref.current
+}
