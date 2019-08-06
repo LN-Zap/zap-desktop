ec37abbac1d9c7456748c88a70776d83f40b7d85
Alexey Padalko <korhaliv@protonmail.com>
2019-08-06 19:12:18 +0300

    feat(ui): add verify message capability

--- renderer/components/Profile/ProfileMenu/ProfileMenu.js
+++ renderer/components/Profile/ProfileMenu/ProfileMenu.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { FormattedMessage } from 'react-intl'
 import { Menu } from 'components/UI'
-import { PANE_NODEINFO, PANE_LNDCONNECT, PANE_SIGNMESSAGE } from '../constants'
+import { PANE_NODEINFO, PANE_LNDCONNECT, PANE_SIGNMESSAGE, PANE_VERIFYMESSAGE } from '../constants'
 import messages from './messages'
 
 const ProfileMenu = ({ group, setGroup, isLocalWallet, ...rest }) => {
@@ -20,13 +20,18 @@ const ProfileMenu = ({ group, setGroup, isLocalWallet, ...rest }) => {
 
   const signMessageLink = {
     id: PANE_SIGNMESSAGE,
-    title: <FormattedMessage {...messages.sign_message_pane_connect_title} />,
+    title: <FormattedMessage {...messages.sign_message_title} />,
     onClick: () => setGroup(PANE_SIGNMESSAGE),
+  }
+  const verifyMessageLink = {
+    id: PANE_VERIFYMESSAGE,
+    title: <FormattedMessage {...messages.verify_message_title} />,
+    onClick: () => setGroup(PANE_VERIFYMESSAGE),
   }
 
   // Get set of menu links based on wallet type.
-  const getLocalLinks = () => [nodeInfoLink, signMessageLink]
-  const getRemoteLinks = () => [nodeInfoLink, connectLink, signMessageLink]
+  const getLocalLinks = () => [nodeInfoLink, signMessageLink, verifyMessageLink]
+  const getRemoteLinks = () => [nodeInfoLink, connectLink, signMessageLink, verifyMessageLink]
   const items = isLocalWallet ? getLocalLinks() : getRemoteLinks()
 
   return <Menu items={items} selectedItem={group} {...rest} />
