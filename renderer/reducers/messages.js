7b6a7966413dc72e10bc873fb56982dcc8fa3310
Alexey Padalko <korhaliv@protonmail.com>
2019-08-13 18:12:40 +0300

    chore(i18n): add new translation strings

---
+++ renderer/reducers/messages.js
@@ -0,0 +1,14 @@
+import { defineMessages } from 'react-intl'
+
+// use pattern [reducer]_message
+
+/* eslint-disable max-len */
+export default defineMessages({
+  backup_import_success: 'Wallet backup imported successfully',
+  backup_import_error: 'Backup import has failed: {error} ',
+  backup_not_found_error: 'Unable to find backup file ',
+  address_new_address_error: 'Unable to get {addressType} address: {error}',
+  settings_init_error: 'Unable to load settings: {error}',
+  payment_send_error: 'Unable to send payment: Invalid invoice (no payment hash)',
+  channels_open_warning: 'Channel opening initiated',
+})
