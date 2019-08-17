84f0bd91bcf28a3d99262c109e5ca360f2747ec9
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-08-17 10:35:35 +0200

    docs(storybook): add form element autofocus examples

--- stories/forms/form.stories.js
+++ stories/forms/form.stories.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { createRef } from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { Box } from 'rebass'
 import { Button, MainContent, Page } from 'components/UI'
 import {
   Checkbox,
+  CurrencyFieldGroup,
   CryptoAmountInput,
   FiatAmountInput,
   Form,
@@ -39,7 +40,7 @@ storiesOf('Forms', module)
   .add('Input', () => (
     <>
       <Form mb={4}>
-        <Input field="fieldName" id="field-name" />
+        <Input field="fieldName" id="field-name" willAutoFocus />
       </Form>
       <Form mb={4}>
         <Input field="fieldName" id="field-name" label="Field with Label" />
@@ -85,7 +86,7 @@ storiesOf('Forms', module)
   .add('IntegerInput', () => (
     <>
       <Form mb={4}>
-        <IntegerInput field="fieldName" id="field-name" label="Number field" />
+        <IntegerInput field="fieldName" id="field-name" label="Number field" willAutoFocus />
       </Form>
       <Form mb={4}>
         <IntegerInput
@@ -109,7 +110,7 @@ storiesOf('Forms', module)
   .add('TextArea', () => (
     <>
       <Form mb={4}>
-        <TextArea field="fieldName" id="field-name" />
+        <TextArea field="fieldName" id="field-name" willAutoFocus />
       </Form>
       <Form mb={4}>
         <TextArea field="fieldName" id="field-name" label="TextArea with Label" />
@@ -154,7 +155,13 @@ storiesOf('Forms', module)
   .add('CryptoAmountInput', () => (
     <Form>
       <Box my={4}>
-        <CryptoAmountInput cryptoUnit="btc" field="cryptoBtc" label="BTC" width={150} />
+        <CryptoAmountInput
+          cryptoUnit="btc"
+          field="cryptoBtc"
+          label="BTC"
+          width={150}
+          willAutoFocus
+        />
       </Box>
 
       <Box my={4}>
@@ -169,7 +176,31 @@ storiesOf('Forms', module)
   .add('FiatAmountInput', () => (
     <Form>
       <Box my={4}>
-        <FiatAmountInput currency="usd" field="fiat" label="USD" width={150} />
+        <FiatAmountInput
+          currency="USD"
+          currentTicker={{}}
+          field="fiat"
+          label="USD"
+          width={150}
+          willAutoFocus
+        />
+      </Box>
+    </Form>
+  ))
+  .add('CurrencyFieldGroup', () => (
+    <Form>
+      <Box my={4}>
+        <CurrencyFieldGroup
+          cryptoUnit="btc"
+          cryptoUnits={[{ key: 'btc', name: 'BTC' }]}
+          currentTicker={{}}
+          fiatCurrencies={[]}
+          fiatCurrency="USD"
+          field="currency"
+          setCryptoCurrency={val => console.log('setCryptoCurrency', val)}
+          setFiatCurrency={val => console.log('setFiatCurrency', val)}
+          willAutoFocus
+        />
       </Box>
     </Form>
   ))
@@ -185,6 +216,7 @@ storiesOf('Forms', module)
             network="testnet"
             validateOnBlur
             validateOnChange
+            willAutoFocus
           />
         </Form>
       </Box>
@@ -211,6 +243,7 @@ storiesOf('Forms', module)
         label="Connection String"
         validateOnBlur
         validateOnChange
+        willAutoFocus
       />
     </Form>
   ))
@@ -223,6 +256,7 @@ storiesOf('Forms', module)
           label="Node Pubkey"
           validateOnBlur
           validateOnChange
+          willAutoFocus
         />
       </Form>
     </Box>
@@ -307,144 +341,148 @@ storiesOf('Forms', module)
       />
     </Form>
   ))
-  .add('Example form', () => (
-    <Page>
-      <MainContent>
-        <Form>
-          {({ formState }) => (
-            <React.Fragment>
-              <Box my={4}>
-                <Input
-                  field="input1"
-                  id="field-name"
-                  label="Example Field"
-                  placeholder="Type here"
-                  validate={validate}
-                  validateOnBlur
-                  validateOnChange
-                />
-              </Box>
-
-              <Box my={4}>
-                <TextArea
-                  field="textarea1"
-                  label="Example TextArea"
-                  placeholder="Type here"
-                  validate={validate}
-                  validateOnBlur
-                  validateOnChange
-                />
-              </Box>
-
-              <Box my={4}>
-                <LightningInvoiceInput
-                  chain="bitcoin"
-                  field="testnet"
-                  id="testnet"
-                  label="Bitcoin or Lightning address (testnet)"
-                  network="testnet"
-                  validateOnBlur
-                  validateOnChange
-                />
-              </Box>
-              <Box>
-                <LightningInvoiceInput
-                  chain="bitcoin"
-                  field="mainnet"
-                  id="mainnet"
-                  label="Bitcoin or Lightning address (mainnet)"
-                  network="mainnet"
-                  validateOnBlur
-                  validateOnChange
-                />
-              </Box>
-
-              <Box my={4}>
-                <NodePubkeyInput
-                  field="pubkey"
-                  id="pubkey"
-                  label="Node Pubkey"
-                  validateOnBlur
-                  validateOnChange
-                />
-              </Box>
-
-              <Box my={4}>
-                <Box>
-                  <Label htmlFor="selectfield1" mb={2}>
-                    Example Select
-                  </Label>
-                </Box>
-                <Box>
-                  <Select
-                    field="selectfield1"
-                    items={selectItems}
-                    onChange={action('change')}
+  .add('Example form', () => {
+    const ref = createRef()
+    return (
+      <Page>
+        <MainContent>
+          <Form>
+            {({ formState }) => (
+              <React.Fragment>
+                <Box my={4}>
+                  <Input
+                    field="input1"
+                    forwardedRef={ref}
+                    label="Example Field"
+                    placeholder="Type here"
                     validate={validate}
                     validateOnBlur
+                    validateOnChange
+                    willAutoFocus
                   />
                 </Box>
-              </Box>
 
-              <Box my={4}>
-                <RadioGroup field="radio">
-                  <Radio description="Radio buttons" label="Item 1" mb={3} value="item1" />
-                  <Radio
-                    description="can have an optional title"
-                    label="Item 2"
-                    mb={3}
-                    value="item2"
-                  />
-                  <Radio description="and description" label="Item 3" mb={3} value="item3" />
-                </RadioGroup>
-              </Box>
-
-              <Box my={4}>
-                <Box>
-                  <Label htmlFor="checkbox1" mb={2}>
-                    Example Toggle
-                  </Label>
-                </Box>
-                <Box>
-                  <Toggle field="checkbox1" label="Example Toggle" onChange={action('change')} />
-                </Box>
-              </Box>
-
-              <Box my={4}>
-                <Box>
-                  <Label htmlFor="slider1" mb={2}>
-                    Example Range
-                  </Label>
-                </Box>
-                <Box>
-                  <Range field="slider1" initialValue={25} onChange={action('change')} />
-                </Box>
-              </Box>
-
-              <Box my={4}>
-                <Box>
-                  <Label htmlFor="checkbox2" mb={2}>
-                    Example Checkbox
-                  </Label>
-                </Box>
-                <Box>
-                  <Checkbox
-                    field="checkbox2"
-                    label="I agree that Zap is the best lightning wallet"
+                <Box my={4}>
+                  <TextArea
+                    field="textarea1"
+                    label="Example TextArea"
+                    placeholder="Type here"
+                    validate={validate}
+                    validateOnBlur
+                    validateOnChange
                   />
                 </Box>
-              </Box>
 
-              <Box my={4}>
-                <Button>Submit</Button>
-              </Box>
+                <Box my={4}>
+                  <LightningInvoiceInput
+                    chain="bitcoin"
+                    field="testnet"
+                    id="testnet"
+                    label="Bitcoin or Lightning address (testnet)"
+                    network="testnet"
+                    validateOnBlur
+                    validateOnChange
+                  />
+                </Box>
+                <Box>
+                  <LightningInvoiceInput
+                    chain="bitcoin"
+                    field="mainnet"
+                    id="mainnet"
+                    label="Bitcoin or Lightning address (mainnet)"
+                    network="mainnet"
+                    validateOnBlur
+                    validateOnChange
+                  />
+                </Box>
 
-              <Box bg="tertiaryColor">
-                <pre>{JSON.stringify(formState, null, 2)}</pre>
-              </Box>
-            </React.Fragment>
-          )}
-        </Form>
-      </MainContent>
-    </Page>
-  ))
+                <Box my={4}>
+                  <NodePubkeyInput
+                    field="pubkey"
+                    id="pubkey"
+                    label="Node Pubkey"
+                    validateOnBlur
+                    validateOnChange
+                  />
+                </Box>
+
+                <Box my={4}>
+                  <Box>
+                    <Label htmlFor="selectfield1" mb={2}>
+                      Example Select
+                    </Label>
+                  </Box>
+                  <Box>
+                    <Select
+                      field="selectfield1"
+                      items={selectItems}
+                      onChange={action('change')}
+                      validate={validate}
+                      validateOnBlur
+                    />
+                  </Box>
+                </Box>
+
+                <Box my={4}>
+                  <RadioGroup field="radio">
+                    <Radio description="Radio buttons" label="Item 1" mb={3} value="item1" />
+                    <Radio
+                      description="can have an optional title"
+                      label="Item 2"
+                      mb={3}
+                      value="item2"
+                    />
+                    <Radio description="and description" label="Item 3" mb={3} value="item3" />
+                  </RadioGroup>
+                </Box>
+
+                <Box my={4}>
+                  <Box>
+                    <Label htmlFor="checkbox1" mb={2}>
+                      Example Toggle
+                    </Label>
+                  </Box>
+                  <Box>
+                    <Toggle field="checkbox1" label="Example Toggle" onChange={action('change')} />
+                  </Box>
+                </Box>
+
+                <Box my={4}>
+                  <Box>
+                    <Label htmlFor="slider1" mb={2}>
+                      Example Range
+                    </Label>
+                  </Box>
+                  <Box>
+                    <Range field="slider1" initialValue={25} onChange={action('change')} />
+                  </Box>
+                </Box>
+
+                <Box my={4}>
+                  <Box>
+                    <Label htmlFor="checkbox2" mb={2}>
+                      Example Checkbox
+                    </Label>
+                  </Box>
+                  <Box>
+                    <Checkbox
+                      field="checkbox2"
+                      label="I agree that Zap is the best lightning wallet"
+                    />
+                  </Box>
+                </Box>
+
+                <Box my={4}>
+                  <Button>Submit</Button>
+                </Box>
+
+                <Box bg="tertiaryColor">
+                  <pre>{JSON.stringify(formState, null, 2)}</pre>
+                </Box>
+              </React.Fragment>
+            )}
+          </Form>
+        </MainContent>
+      </Page>
+    )
+  })
