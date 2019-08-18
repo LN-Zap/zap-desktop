7f7f0de9ac4ba36af596fd0a1689bf592329b75b
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-08-18 14:07:39 +0200

    test(unit): add network reducer tests

---
+++ test/unit/reducers/network.spec.js
@@ -0,0 +1,37 @@
+import snapshotDiff from '../__helpers__/snapshotDiff'
+import reducer, {
+  GET_DESCRIBE_NETWORK,
+  RECEIVE_DESCRIBE_NETWORK,
+  UPDATE_NODE_DATA,
+} from 'reducers/network'
+
+describe('reducers', () => {
+  describe('networkReducer', () => {
+    it('should handle initial state', () => {
+      expect(reducer(undefined, {})).toMatchSnapshot()
+    })
+
+    it('should handle GET_DESCRIBE_NETWORK', () => {
+      const action = {
+        type: GET_DESCRIBE_NETWORK,
+      }
+      expect(snapshotDiff(reducer(undefined, {}), reducer(undefined, action))).toMatchSnapshot()
+    })
+
+    it('should handle RECEIVE_DESCRIBE_NETWORK', () => {
+      const action = {
+        type: RECEIVE_DESCRIBE_NETWORK,
+        nodes: [{ some: 'data' }],
+      }
+      expect(snapshotDiff(reducer(undefined, {}), reducer(undefined, action))).toMatchSnapshot()
+    })
+
+    it('should handle UPDATE_NODE_DATA', () => {
+      const action = {
+        type: UPDATE_NODE_DATA,
+        data: [{ some: 'data' }],
+      }
+      expect(snapshotDiff(reducer(undefined, {}), reducer(undefined, action))).toMatchSnapshot()
+    })
+  })
+})
