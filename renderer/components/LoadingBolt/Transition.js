bd975bdf6279ff3f7c6a58c9e158ffdea7aba291
Alexey Padalko <korhaliv@protonmail.com>
2019-09-17 16:53:38 +0300

    refactor: extract loading Transition component

---
+++ renderer/components/LoadingBolt/Transition.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { animated, Transition as RPTransition } from 'react-spring/renderprops.cjs'
+
+const AnimationContainer = styled(animated.div)`
+  z-index: 1000;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`
+
+const Transition = ({ isLoading, children }) => {
+  return (
+    <RPTransition
+      config={{ duration: 500, delay: 500 }}
+      enter={{ opacity: 1 }}
+      from={{ opacity: 1 }}
+      items={isLoading}
+      leave={{ opacity: 0 }}
+      native
+    >
+      {show =>
+        show &&
+        // eslint-disable-next-line react/display-name
+        (springStyles => <AnimationContainer style={springStyles}>{children}</AnimationContainer>)
+      }
+    </RPTransition>
+  )
+}
+
+Transition.propTypes = {
+  children: PropTypes.node,
+  isLoading: PropTypes.bool,
+}
+
+export default Transition
