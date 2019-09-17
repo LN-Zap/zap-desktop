5194a159c2a9c3667aa92af729f21fbe8de8ae4e
Alexey Padalko <korhaliv@protonmail.com>
2019-09-17 17:30:09 +0300

    refactor: extract loading Placeholder component

---
+++ renderer/components/LoadingBolt/Placeholder.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+import { Box } from 'rebass/styled-components'
+import { themeGet } from '@styled-system/theme-get'
+import { lighten } from 'polished'
+
+const pulse = props => keyframes`
+0% {
+  background-color: ${lighten(0.03, themeGet('colors.' + props.color)(props))};
+}
+50% {
+  background-color: ${lighten(0.1, themeGet('colors.' + props.color)(props))};
+}
+100% {
+  background-color: ${lighten(0.03, themeGet('colors.' + props.color)(props))};
+}
+`
+
+const PlaceholderBox = props => <Box sx={{ borderRadius: 'xl' }} {...props} />
+
+const Placeholder = styled(PlaceholderBox)`
+  animation: ${pulse} 2s linear infinite;
+`
+
+Placeholder.defaultProps = {
+  color: 'secondaryColor',
+}
+
+export default Placeholder
