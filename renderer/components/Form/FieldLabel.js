224116a52a6654675f50beb34650f505f87d3227
Alexey Padalko <korhaliv@protonmail.com>
2019-09-19 15:05:29 +0300

    refactor: add ability to specify tooltip for Row and Field labels

--- renderer/components/Form/FieldLabel.js
+++ renderer/components/Form/FieldLabel.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import RowLabel from './RowLabel'
 
 const FieldLabelFactory = messages => {
-  const FieldLabel = ({ itemKey, ...rest }) => {
+  const FieldLabel = ({ itemKey, tooltip, ...rest }) => {
     const messageKey = itemKey.replace('.', '_')
     return (
       <RowLabel
@@ -11,12 +11,14 @@ const FieldLabelFactory = messages => {
         descMessage={messages[`${messageKey}_description`]}
         htmlFor={itemKey}
         nameMessage={messages[`${messageKey}_label`]}
+        tooltipMessage={messages[tooltip]}
       />
     )
   }
 
   FieldLabel.propTypes = {
     itemKey: PropTypes.string.isRequired,
+    tooltip: PropTypes.string,
   }
 
   return FieldLabel
--- renderer/components/Form/RowLabel.js
+++ renderer/components/Form/RowLabel.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Box } from 'rebass/styled-components'
+import { Box, Flex } from 'rebass/styled-components'
 import { FormattedMessage } from 'react-intl'
-import { Text } from 'components/UI'
+import { Text, Tooltip } from 'components/UI'
 import Label from './Label'
 
-const RowLabel = ({ nameMessage, descMessage, htmlFor, ...rest }) => {
+const RowLabel = ({ tooltipMessage, nameMessage, descMessage, htmlFor, ...rest }) => {
   return (
     <Box {...rest}>
-      <Label htmlFor={htmlFor} mb={2}>
-        <FormattedMessage {...nameMessage} />
-      </Label>
+      <Flex>
+        <Label htmlFor={htmlFor} mb={2}>
+          <FormattedMessage {...nameMessage} />
+        </Label>
+        {tooltipMessage && (
+          <Tooltip ml={1} mt="2px">
+            <FormattedMessage {...tooltipMessage} />
+          </Tooltip>
+        )}
+      </Flex>
+
       <Text color="gray" fontWeight="light">
         <FormattedMessage {...descMessage} />
       </Text>
@@ -22,6 +30,7 @@ RowLabel.propTypes = {
   descMessage: PropTypes.object.isRequired,
   htmlFor: PropTypes.string,
   nameMessage: PropTypes.object.isRequired,
+  tooltipMessage: PropTypes.object,
 }
 
 export default RowLabel
