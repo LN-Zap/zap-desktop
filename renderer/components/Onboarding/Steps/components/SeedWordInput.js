0fd35e50420723aaa89a438612b22472e9f5257b
Umar Bolatov <bolatovumar@gmail.com>
2019-09-22 15:30:55 -0700

    feat(ui): add autosuggest for seed recovery
    address #2765

---
+++ renderer/components/Onboarding/Steps/components/SeedWordInput.js
@@ -0,0 +1,111 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Downshift from 'downshift'
+import bip39 from 'bip39-en'
+import { Box, Flex } from 'rebass/styled-components'
+import { useFieldApi } from 'informed'
+import { useMaxScreenHeight } from 'hooks'
+import SeedWordList from './SeedWordList'
+import { Input, Label } from 'components/Form'
+
+const SeedWordInput = ({ index, onPaste, placeholder, setFormApiFieldValue }) => {
+  const inputKey = `word${index}`
+  const fieldApi = useFieldApi(inputKey)
+  const [measuredRef, maxHeight] = useMaxScreenHeight(300)
+  const height = Math.max(maxHeight, 100)
+
+  /**
+   * mask - Masks the value with a trimmed version.
+   *
+   * @param {string} value Value to check
+   * @returns {string} Trimmed string
+   */
+  const mask = value => (value ? value.trim() : value)
+
+  /**
+   * validate - Check if a word is included in the bip39 word list.
+   *
+   * @param {string} value Value to check
+   * @returns {string|undefined} undefined if word is in bip39 word list. The string 'incorrect' otherwise
+   */
+  const validate = value => {
+    return bip39.includes(value) ? undefined : 'incorrect'
+  }
+
+  /**
+   * getValue - Get the current field value.
+   *
+   * @returns {string} Current field value
+   */
+  const getValue = () => {
+    return fieldApi.getValue() || ''
+  }
+
+  /**
+   * onChange - Set the current field value.
+   *
+   * @param {Event} event onChange event
+   */
+  const onChange = event => {
+    fieldApi.setValue(event.target.value)
+  }
+
+  return (
+    <Downshift onSelect={setFormApiFieldValue}>
+      {({
+        getInputProps,
+        getItemProps,
+        getMenuProps,
+        getRootProps,
+        isOpen,
+        inputValue,
+        highlightedIndex,
+      }) => (
+        <Box sx={{ position: 'relative' }} {...getRootProps()}>
+          <Flex alignItems="center" as="li" justifyContent="flex-start" my={1}>
+            <Label htmlFor={inputKey} mb={0} width={35}>
+              {index}
+            </Label>
+            <Input
+              {...getInputProps({
+                field: inputKey,
+                hasMessage: false,
+                highlightOnValid: true,
+                onChange,
+                mask,
+                onPaste,
+                placeholder,
+                validate,
+                validateOnChange: true,
+                value: getValue(),
+                variant: 'thin',
+                willAutoFocus: index === 1,
+              })}
+            />
+          </Flex>
+          {isOpen && inputValue && (
+            <SeedWordList
+              {...{
+                getMenuProps,
+                highlightedIndex,
+                getItemProps,
+                inputValue,
+              }}
+              ref={measuredRef}
+              maxHeight={height}
+            />
+          )}
+        </Box>
+      )}
+    </Downshift>
+  )
+}
+
+SeedWordInput.propTypes = {
+  index: PropTypes.number.isRequired,
+  onPaste: PropTypes.func,
+  placeholder: PropTypes.string.isRequired,
+  setFormApiFieldValue: PropTypes.func.isRequired,
+}
+
+export default SeedWordInput
---
+++ renderer/components/Onboarding/Steps/components/SeedWordList.js
@@ -0,0 +1,111 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import bip39 from 'bip39-en'
+import { Box, Flex } from 'rebass/styled-components'
+import { Text } from 'components/UI'
+
+/**
+ * Create a mapping of first letter to words in BIP 39 list so
+ * we don't have to filter through the whole array when looking up values
+ * every time
+ */
+const bip39WordMap = bip39.reduce((bip39WordMap, word) => {
+  const [firstLetter] = word
+  if (!bip39WordMap[firstLetter]) {
+    bip39WordMap[firstLetter] = []
+  }
+
+  bip39WordMap[firstLetter].push(word)
+
+  return bip39WordMap
+}, {})
+
+const StyledOptionList = React.forwardRef((props, ref) => (
+  <Box
+    ref={ref}
+    as="ul"
+    bg="secondaryColor"
+    maxHeight={300}
+    mt={1}
+    p={0}
+    sx={{
+      position: 'absolute',
+      zIndex: 2,
+      overflowY: 'auto',
+      overflowX: 'hidden',
+      outline: 0,
+      transition: 'opacity 0.1s ease',
+      borderRadius: 's',
+      boxShadow: 's',
+    }}
+    width={1}
+    {...props}
+  />
+))
+StyledOptionList.displayName = 'StyledOptionList'
+
+const SeedWordList = React.forwardRef(
+  ({ highlightedIndex, getItemProps, getMenuProps, inputValue, ...rest }, ref) => {
+    // Don't show suggestions if user didn't input anything
+    if (!inputValue) {
+      return null
+    }
+
+    // Get the wordlist we can suggest
+    const [firstLetter] = inputValue
+    const suggestions = bip39WordMap[firstLetter]
+    if (!suggestions) {
+      return null
+    }
+
+    // Get the words that match the input
+    const wordList = suggestions.filter(word => word.indexOf(inputValue) === 0)
+
+    // Input doesn't match anything, show nothing
+    if (!wordList.length) {
+      return null
+    }
+
+    // We have one suugestion only and it matches the input exactly.
+    // No point to annoy the user by showing exactly the same thing so
+    // we don't show this single suggestion.
+    if (wordList.length === 1 && wordList[0] === inputValue) {
+      return null
+    }
+
+    return (
+      <StyledOptionList {...getMenuProps(rest, { suppressRefError: true })} ref={ref}>
+        {wordList.map((item, index) => (
+          <Flex
+            {...getItemProps({
+              index,
+              item,
+            })}
+            key={item}
+            alignItems="center"
+            as="li"
+            bg={highlightedIndex === index ? 'primaryColor' : null}
+            p={2}
+            sx={{
+              outline: 'none',
+              cursor: 'pointer',
+            }}
+          >
+            <Text>{item}</Text>
+          </Flex>
+        ))}
+      </StyledOptionList>
+    )
+  }
+)
+
+SeedWordList.displayName = 'SeedWordList'
+
+SeedWordList.propTypes = {
+  getItemProps: PropTypes.func.isRequired,
+  getMenuProps: PropTypes.func.isRequired,
+  highlightedIndex: PropTypes.number,
+  inputValue: PropTypes.string.isRequired,
+}
+
+export default SeedWordList
