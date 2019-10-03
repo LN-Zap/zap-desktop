56fcafe092f4db9694216655f5ad8c56b557094a
Alexey Padalko <korhaliv@protonmail.com>
2019-10-03 11:03:46 +0300

    fix(ui): close channel modal on channel open completion

--- renderer/containers/Channels/ChannelCreate.js
+++ renderer/containers/Channels/ChannelCreate.js
@@ -1,6 +1,9 @@
 import { connect } from 'react-redux'
 import ChannelCreate from 'components/Channels/ChannelCreate'
+import { closeModal } from 'reducers/modal'
 import { updateContactFormSearchQuery, contactFormSelectors } from 'reducers/contactsform'
+
+const onSubmit = () => closeModal()
 
 const mapStateToProps = state => ({
   searchQuery: state.contactsform.searchQuery,
@@ -9,6 +12,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = {
   updateContactFormSearchQuery,
+  onSubmit,
 }
 
 export default connect(
