385ed2a8374debaf3a409d90b461524b4f19cc5a
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-10-09 13:56:16 +0200

    style(lint): downgrade lint errors to warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,40 @@
 var path = require('path')
 
+const downgraded = [
+  'array-callback-return',
+  'class-methods-use-this',
+  'consistent-return',
+  'default-case',
+  'eqeqeq',
+  'getter-return',
+  'global-require',
+  'new-cap',
+  'no-async-promise-executor',
+  'no-await-in-loop',
+  'no-lonely-if',
+  'no-multi-str',
+  'no-nested-ternary',
+  'no-plusplus',
+  'no-prototype-builtins',
+  'no-restricted-globals',
+  'no-restricted-syntax',
+  'no-return-assign',
+  'no-return-await',
+  'no-self-compare',
+  'no-shadow',
+  'no-underscore-dangle',
+  'no-unused-expressions',
+  'no-use-before-define',
+  'prefer-const',
+  'require-atomic-updates',
+]
+
 module.exports = {
   extends: '@ln-zap',
+  rules: downgraded.reduce((acc, next) => {
+    acc[next] = 'warn'
+    return acc
+  }, {}),
   settings: {
     'import/resolver': {
       'babel-module': {},
