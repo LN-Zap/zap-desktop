4986acb3be91918c324a64fe87c8d8aa898525f1
Alexey Padalko <korhaliv@protonmail.com>
2019-10-16 17:04:59 +0300

    chore(i18n): add translations

--- renderer/components/Settings/Security/messages.js
+++ renderer/components/Settings/Security/messages.js
@@ -4,14 +4,18 @@ import { defineMessages } from 'react-intl'
 export default defineMessages({
   password_dialog_cancel_button_text: 'Cancel',
   password_prompt_dialog_ok_button_text: 'Ok',
+  password_set_dialog_ok_button_text: 'Set',
   password_change_dialog_header: 'Change password',
   password_prompt_dialog_header: 'Enter app password',
   password_set_dialog_header: 'Set app password',
   password_set_dialog_description: 'Must be at least 6 characters.',
+  password_do_not_match: `Password don't match`,
   password_prompt_dialog_description: 'Your app password.',
   password_change_button_text: 'Change',
   password_enable_button_text: 'Enable',
   password_disable_button_text: 'Disable',
   password_new_password: 'New password.',
+  password_confirm_password: 'Confirm password.',
+  password: 'Password.',
   password_old_password: 'Old password.',
 })
