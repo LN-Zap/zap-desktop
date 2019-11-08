0121b5d9ec480e8e86a1305805b6a1bdaf1f272d
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-11-08 13:57:11 +0100

    fix(ui): correct expire date for invoices with far future expiry
    Refactor the Countdown component to a functional component and use
    `useTimout` hook to handle updating the expired state at the correct
    time.
    
    Fix #3144

--- renderer/components/UI/Countdown.js
+++ renderer/components/UI/Countdown.js
@@ -1,94 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { FormattedMessage, FormattedRelativeTime } from 'react-intl'
+import { useTimeout } from 'hooks'
 import Text from './Text'
 import messages from './messages'
 
-class Countdown extends React.Component {
-  state = {
-    isExpired: null,
-    timer: null,
+const Countdown = ({
+  offset,
+  onExpire,
+  colorActive,
+  colorExpired,
+  countdownStyle,
+  isContinual,
+  updateInterval,
+  ...rest
+}) => {
+  let expiresIn = offset
+  if (offset instanceof Date) {
+    expiresIn = offset - Date.now()
+    expiresIn = Math.round(expiresIn / 1000)
   }
 
-  static propTypes = {
-    colorActive: PropTypes.string,
-    colorExpired: PropTypes.string,
-    countdownStyle: PropTypes.string,
-    isContinual: PropTypes.bool,
-    offset: PropTypes.oneOfType([PropTypes.number, PropTypes.instanceOf(Date)]).isRequired,
-    onExpire: PropTypes.func,
-    updateInterval: PropTypes.number,
-  }
+  const [isExpired, setIsExpired] = useState(expiresIn <= 0)
 
-  static defaultProps = {
-    colorActive: 'superGreen',
-    colorExpired: 'superRed',
-    countdownStyle: 'long',
-    isContinual: true,
-    updateInterval: 1,
-  }
+  useTimeout(() => {
+    setIsExpired(true)
+    onExpire && onExpire()
+  }, expiresIn * 1000)
 
-  componentDidMount() {
-    const { offset, onExpire } = this.props
-    let expiresIn = offset
+  return (
+    <Text color={isExpired ? colorExpired : colorActive} {...rest}>
+      {isExpired ? (
+        <FormattedMessage {...messages.expired} />
+      ) : (
+        <FormattedMessage {...messages.expires} />
+      )}
+      {(!isExpired || (isExpired && isContinual)) && (
+        <>
+          <i> </i>
+          <FormattedRelativeTime
+            style={countdownStyle}
+            updateIntervalInSeconds={updateInterval}
+            value={expiresIn}
+          />
+        </>
+      )}
+    </Text>
+  )
+}
 
-    if (offset instanceof Date) {
-      expiresIn = offset - Date.now()
-      this.expiresIn = Math.round(expiresIn / 1000)
-    } else {
-      this.expiresIn = expiresIn
-    }
+Countdown.propTypes = {
+  colorActive: PropTypes.string,
+  colorExpired: PropTypes.string,
+  countdownStyle: PropTypes.string,
+  isContinual: PropTypes.bool,
+  offset: PropTypes.oneOfType([PropTypes.number, PropTypes.instanceOf(Date)]).isRequired,
+  onExpire: PropTypes.func,
+  updateInterval: PropTypes.number,
+}
 
-    if (expiresIn >= 0) {
-      this.setState({ isExpired: false })
-      const timer = setInterval(() => {
-        this.setState({ isExpired: true })
-        onExpire && onExpire()
-      }, this.expiresIn * 1000)
-      this.setState({ timer })
-    } else {
-      this.setState({ isExpired: true })
-      onExpire && onExpire()
-    }
-  }
-
-  componentWillUnmount() {
-    const { timer } = this.state
-    clearInterval(timer)
-  }
-
-  render() {
-    const {
-      colorActive,
-      colorExpired,
-      countdownStyle,
-      isContinual,
-      offset,
-      onExpire,
-      updateInterval,
-      ...rest
-    } = this.props
-    const { isExpired } = this.state
-    return (
-      <Text color={isExpired ? colorExpired : colorActive} {...rest}>
-        {isExpired ? (
-          <FormattedMessage {...messages.expired} />
-        ) : (
-          <FormattedMessage {...messages.expires} />
-        )}
-        {(!isExpired || (isExpired && isContinual)) && (
-          <>
-            &nbsp;
-            <FormattedRelativeTime
-              style={countdownStyle}
-              updateIntervalInSeconds={updateInterval}
-              value={this.expiresIn}
-            />
-          </>
-        )}
-      </Text>
-    )
-  }
+Countdown.defaultProps = {
+  colorActive: 'superGreen',
+  colorExpired: 'superRed',
+  countdownStyle: 'long',
+  isContinual: true,
+  updateInterval: 1,
 }
 
 export default Countdown
