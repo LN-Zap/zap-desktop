a720892a4a4f15713d243fe1640db3640a3dd47d
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-11-09 21:42:07 +0100

    style: fix typos

--- renderer/reducers/invoice.js
+++ renderer/reducers/invoice.js
@@ -65,7 +65,7 @@ const decorateInvoice = invoice => {
   // Add a `finalAmount` prop which shows the amount paid if set, or the invoice value if not.
   decoration.finalAmount = invoice.amt_paid_sat ? invoice.amt_paid_sat : invoice.value
 
-  // Add an `isExpired` prop which shows wether the invoice is expired or not.
+  // Add an `isExpired` prop which shows whether the invoice is expired or not.
   const expiresAt = parseInt(invoice.creation_date, 10) + parseInt(invoice.expiry, 10)
   decoration.isExpired = expiresAt < Math.round(new Date() / 1000)
 
