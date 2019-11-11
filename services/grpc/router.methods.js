8c37feedfe99e166996e0c2e0ac2232d72dde6fc
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-11-11 09:45:22 +0100

    refactor: add Router probePayment helper

--- services/grpc/router.methods.js
+++ services/grpc/router.methods.js
@@ -1,9 +1,54 @@
 import { grpcLog } from '@zap/utils/log'
 import { logGrpcCmd } from './helpers'
 
+const PAYMENT_PROBE_TIMEOUT = 15
+
 // ------------------------------------
 // Wrappers / Overrides
 // ------------------------------------
+
+/**
+ * probePayment - Call lnd grpc sendPayment method with a fake payment hash in order to probe for a route.
+ *
+ * @param {string} dest Identity pubkey of the payment recipient
+ * @param {number} amt Number of satoshis to send
+ * @param {number} timeout Upper limit on the amount of time spent attempting to fulfill the payment
+ * @returns {Promise} The taken route when state is SUCCEEDED.
+ */
+async function probePayment(dest, amt, timeout = PAYMENT_PROBE_TIMEOUT) {
+  logGrpcCmd('Router.probePayment', { dest, amt, timeout })
+
+  const payload = {
+    payment_hash: Buffer.from('0'),
+    dest: Buffer.from(dest, 'hex'),
+    amt,
+    timeout_seconds: timeout,
+  }
+
+  return new Promise((resolve, reject) => {
+    const call = this.service.sendPayment(payload)
+
+    call.on('data', data => {
+      grpcLog.debug('PROBE DATA :%o', data)
+
+      switch (data.state) {
+        case 'IN_FLIGHT':
+          // Payment is still in progress, do nothing.
+          grpcLog.debug('PROBE IN_FLIGHT...')
+          break
+
+        case 'FAILED_INCORRECT_PAYMENT_DETAILS':
+          grpcLog.info('PROBE SUCCESS: %o', data)
+          resolve(data.route)
+          break
+
+        default:
+          grpcLog.warn('PROBE FAILED: %o', data)
+          reject(new Error(data.state))
+      }
+    })
+  })
+}
 
 /**
  * sendPayment - Call lnd grpc sendPayment method.
@@ -56,5 +101,6 @@ async function sendPayment(payload = {}) {
 }
 
 export default {
+  probePayment,
   sendPayment,
 }
