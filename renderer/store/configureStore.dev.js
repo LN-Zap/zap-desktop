a416e2eb95f466c77e85f0b1ccf17a715df169c4
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2020-06-11 16:24:31 +0200

    refactor: removing rHash and rPreimage fom redux

--- renderer/store/configureStore.dev.js
+++ renderer/store/configureStore.dev.js
@@ -33,8 +33,8 @@ const composeEnhancers =
                   invoices: invoice.invoices.map(invoice => {
                     return {
                       ...invoice,
-                      // rHash: '<<R_HASH_BUFFER_DATA>>',
-                      // rPreimage: '<<R_PREIMAGE_BUFFER_DATA>>',
+                      rHash: '<<R_HASH_BUFFER_DATA>>',
+                      rPreimage: '<<R_PREIMAGE_BUFFER_DATA>>',
                     }
                   }),
                 },
