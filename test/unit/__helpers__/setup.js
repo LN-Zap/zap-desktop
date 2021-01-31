55b579057399f847addb1004cc2cfa0f72d6ea08
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2021-01-31 20:45:36 +0000

    test(unit): do not require preload script

--- test/unit/__helpers__/setup.js
+++ test/unit/__helpers__/setup.js
@@ -4,9 +4,6 @@ import axios from 'axios'
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-// Import the preload script in order to ensure that global helpers get injected.
-import '@zap/electron/preload'
-
 configure({ adapter: new Adapter() })
 
 jest.setTimeout(25000)
